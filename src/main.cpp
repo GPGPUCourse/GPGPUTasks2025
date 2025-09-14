09456b467d474f6435371a4be610bca98eabe328
Тatiana Shimanskaya <ttnsimanskaa@gmail.com>
2025-09-14 14:31:56 +0300

    solution

--- src/main.cpp
+++ src/main.cpp
@@ -66,19 +66,30 @@ int main()
 		// Затем откройте документацию по clGetPlatformInfo и в секции Errors найдите ошибку, с которой столкнулись
 		// в документации подробно объясняется, какой ситуации соответствует данная ошибка, и это позволит, проверив код, понять, чем же вызвана данная ошибка (некорректным аргументом param_name)
 		// Обратите внимание, что в этом же libs/clew/CL/cl.h файле указаны всевоможные defines, такие как CL_DEVICE_TYPE_GPU и т.п.
+		// OCL_SAFE_CALL(clGetPlatformInfo(platform, 4815, 0, nullptr, &platformNameSize));
 
 		// TODO 1.2
 		// Аналогично тому, как был запрошен список идентификаторов всех платформ - так и с названием платформы, теперь, когда известна длина названия - его можно запросить:
 		std::vector<unsigned char> platformName(platformNameSize, 0);
-		// clGetPlatformInfo(...);
+		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, platformNameSize, platformName.data(), &platformNameSize));
 		std::cout << "    Platform name: " << platformName.data() << std::endl;
 
 		// TODO 1.3
 		// Запросите и напечатайте так же в консоль вендора данной платформы
+		size_t platformVendorSize = 0;
+		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, 0, nullptr, &platformVendorSize));
+
+		std::string platformVendor(platformVendorSize, '\0');
+		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, platformVendorSize, platformVendor.data(), &platformVendorSize));
+		std::cout << "    Platform vendor: " << platformVendor.c_str() << std::endl;
 
 		// TODO 2.1
 		// Запросите число доступных устройств данной платформы (аналогично тому, как это было сделано для запроса числа доступных платформ - см. секцию "OpenCL Runtime" -> "Query Devices")
 		cl_uint devicesCount = 0;
+		OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, nullptr, &devicesCount));
+
+		std::vector<cl_device_id> devices(devicesCount);
+		OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, devicesCount, devices.data(), nullptr));
 
 		for(int deviceIndex = 0; deviceIndex < devicesCount; ++deviceIndex)
 		{
@@ -88,6 +99,54 @@ int main()
 			// - Тип устройства (видеокарта/процессор/что-то странное)
 			// - Размер памяти устройства в мегабайтах
 			// - Еще пару или более свойств устройства, которые вам покажутся наиболее интересными
+
+			cl_device_id device = devices[deviceIndex];
+
+			char deviceName[1024];
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_NAME, sizeof(deviceName), deviceName, nullptr));
+
+			cl_device_type deviceType{};
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_TYPE, sizeof(deviceType), &deviceType, nullptr));
+
+			std::string typeStr{};
+
+			if(deviceType & CL_DEVICE_TYPE_CPU)
+			{
+				typeStr = "CPU";
+			}
+			else if(deviceType & CL_DEVICE_TYPE_GPU)
+			{
+				typeStr = "GPU";
+			}
+			else if(deviceType & CL_DEVICE_TYPE_ACCELERATOR)
+			{
+				typeStr = "Accelerator";
+			}
+			else
+			{
+				typeStr = "Other";
+			}
+
+			cl_ulong globalMemSize{};
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(globalMemSize), &globalMemSize, nullptr));
+
+			cl_uint computeUnits{};
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(computeUnits), &computeUnits, nullptr));
+
+			size_t maxWorkGroupSize{};
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(maxWorkGroupSize), &maxWorkGroupSize, nullptr));
+
+			cl_uint clockFrequency{};
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(clockFrequency), &clockFrequency, nullptr));
+
+			std::cout << "    Device #" << deviceIndex << ":\n"
+			          << "      Name: " << deviceName << "\n"
+			          << "      Type: " << typeStr << "\n"
+			          << "      Global Memory: " << (globalMemSize / (1024 * 1024)) << " MB\n"
+			          << "      Compute Units: " << computeUnits << "\n"
+			          << "      Max Work Group Size: " << maxWorkGroupSize << "\n"
+			          << "      Clock Frequency: " << clockFrequency << " MHz\n"
+			          << std::endl;
 		}
 	}
 
