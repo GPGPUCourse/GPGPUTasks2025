413782e980171bb3698fe56c404f3416db7a914e
NewUser <newuser@example.com>
2025-09-26 23:40:24 +0300

    Solution

--- src/kernels/cu/aplusb_matrix_good.cu
+++ src/kernels/cu/aplusb_matrix_good.cu
@@ -1,16 +1,16 @@
 #include <libgpu/context.h>
-#include <libgpu/work_size.h>
 #include <libgpu/shared_device_buffer.h>
+#include <libgpu/work_size.h>
 
 #include <libgpu/cuda/cu/common.cu>
 
 #include "../defines.h"
 
 __global__ void aplusb_matrix_good(const unsigned int* a,
-                       const unsigned int* b,
-                             unsigned int* c,
-                             unsigned int  width,
-                             unsigned int  height)
+    const unsigned int* b,
+    unsigned int* c,
+    unsigned int width,
+    unsigned int height)
 {
     // все три массива - линейно выложенные двумерные матрицы размера width (число столбиков) x height (число рядов)
     // при этом в памяти подряд идут элементы являющимися соседями в рамках одного ряда,
@@ -19,11 +19,16 @@ __global__ void aplusb_matrix_good(const unsigned int* a,
     // т.е. если в матрице сделать шаг вверх или вниз на одну ячейку - то в памяти мы шагнем на так называемый stride=width*4 байта
 
     // TODO реализуйте этот кернел - просуммируйте две матрицы так чтобы получить максимально ХОРОШУЮ производительность с точки зрения memory coalesced паттерна доступа
+    const unsigned int row = blockIdx.y * blockDim.y + threadIdx.y;
+    const unsigned int column = blockIdx.x * blockDim.x + threadIdx.x;
+    if (row < height && column < width) {
+        c[row * width + column] = a[row * width + column] + b[row * width + column];
+    }
 }
 
 namespace cuda {
-void aplusb_matrix_good(const gpu::WorkSize &workSize,
-            const gpu::gpu_mem_32u &a, const gpu::gpu_mem_32u &b, gpu::gpu_mem_32u &c, unsigned int width, unsigned int height)
+void aplusb_matrix_good(const gpu::WorkSize& workSize,
+    const gpu::gpu_mem_32u& a, const gpu::gpu_mem_32u& b, gpu::gpu_mem_32u& c, unsigned int width, unsigned int height)
 {
     gpu::Context context;
     rassert(context.type() == gpu::Context::TypeCUDA, 34523543124312, context.type());
