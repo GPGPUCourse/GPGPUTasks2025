40afb2cdb47ed8aaf943c202a530fcf4674e564d
Vladimir Vasenev <vasenyov04@outlook.com>
2025-09-29 19:32:06 +0000

    fix output

--- src/main_aplusb_matrix.cpp
+++ src/main_aplusb_matrix.cpp
@@ -89,7 +89,7 @@ void run(int argc, char** argv)
         }
         std::cout << "a + b matrix kernel times (in seconds) - " << stats::valuesStatsLine(times) << std::endl;
 
-        double memory_size_gb = sizeof(unsigned int) * 3 * width * height / (1024.0 * 3);
+        double memory_size_gb = sizeof(unsigned int) * 3 * width * height / 1024.0 / 1024.0 / 1024.0;
         std::cout << "a + b kernel median VRAM bandwidth: " << memory_size_gb / stats::median(times) << " GB/s" << std::endl;
 
         // TODO Считываем результат по PCI-E шине: GPU VRAM -> CPU RAM
@@ -136,7 +136,7 @@ void run(int argc, char** argv)
         }
         std::cout << "a + b matrix kernel times (in seconds) - " << stats::valuesStatsLine(times) << std::endl;
 
-        double memory_size_gb = sizeof(unsigned int) * 3 * width * height / (1024.0 * 3);
+        double memory_size_gb = sizeof(unsigned int) * 3 * width * height / 1024.0 / 1024.0 / 1024.0;
         std::cout << "a + b kernel median VRAM bandwidth: " << memory_size_gb / stats::median(times) << " GB/s" << std::endl;
 
         // TODO Считываем результат по PCI-E шине: GPU VRAM -> CPU RAM
