0a030b51f6c7b2e3914e4d01e3186e98bccfa032
xyliganSereja <matva0509@gmail.com>
2025-10-13 16:18:16 +0300

    Update defines.h

--- src/kernels/defines.h
+++ src/kernels/defines.h
@@ -6,7 +6,7 @@
 #define GROUP_SIZE_Y 16
 
 #define VEC_SIZE_V 11
-#define VEC_SIZE_H 7
+#define VEC_SIZE_H 11
 #define TILE GROUP_SIZE_X
 
 #define RASSERT_ENABLED 0 // disabled by default, enable for debug by changing 0 to 1, disable before performance evaluation/profiling/commiting
