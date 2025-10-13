6d996b2e3bf4fd1792334bb02231d702e7b7595b
Kononov Grigorii <grigorykononov2005@gmail.com>
2025-10-13 21:18:36 +0300

    +transpose

--- src/kernels/cl/matrix_02_transpose_coalesced_via_local_memory.cl
+++ src/kernels/cl/matrix_02_transpose_coalesced_via_local_memory.cl
@@ -4,12 +4,30 @@
 
 #include "../defines.h"
 
-__attribute__((reqd_work_group_size(1, 1, 1)))
+__attribute__((reqd_work_group_size(GROUP_SIZE_X, GROUP_SIZE_Y, 1)))
 __kernel void matrix_02_transpose_coalesced_via_local_memory(
                        __global const float* matrix,            // w x h
                        __global       float* transposed_matrix, // h x w
                                 unsigned int w,
                                 unsigned int h)
 {
-    // TODO
+    __local float tile[GROUP_SIZE_Y][GROUP_SIZE_X + 1];
+    
+    const unsigned int local_x = get_local_id(0);
+    const unsigned int local_y = get_local_id(1);
+    const unsigned int global_x = get_global_id(0);
+    const unsigned int global_y = get_global_id(1);
+    
+    if (global_x < w && global_y < h) {
+        tile[local_y][local_x] = matrix[global_y * w + global_x];
+    }
+    
+    barrier(CLK_LOCAL_MEM_FENCE);
+    
+    unsigned int transposed_x = get_group_id(1) * 16 + local_x;
+    unsigned int transposed_y = get_group_id(0) * 16 + local_y;
+    
+    if (transposed_x < h && transposed_y < w) {
+        transposed_matrix[transposed_y * h + transposed_x] = tile[local_x][local_y];
+    }
 }
--- src/main_01_matrix_transpose.cpp
+++ src/main_01_matrix_transpose.cpp
@@ -32,9 +32,6 @@ void run(int argc, char** argv)
     ocl::KernelSource ocl_matrix01TransposeNaive(ocl::getMatrix01TransposeNaive());
     ocl::KernelSource ocl_matrix02TransposeCoalescedViaLocalMemory(ocl::getMatrix02TransposeCoalescedViaLocalMemory());
 
-    avk2::KernelSource vk_matrix01TransposeNaive(avk2::getMatrix01TransposeNaive());
-    avk2::KernelSource vk_matrix02TransposeCoalescedViaLocalMemory(avk2::getMatrix02TransposeCoalescedViaLocalMemory());
-
     unsigned int ksize = 512;
     unsigned int w = ksize * 32;
     unsigned int h = ksize * 16;
@@ -46,11 +43,8 @@ void run(int argc, char** argv)
         input_cpu[i] = r.nextf();
     }
 
-    // Аллоцируем буферы в VRAM
-    gpu::gpu_mem_32f input_matrix_gpu (h * w); // rows=H x cols=W
-    gpu::gpu_mem_32f output_matrix_gpu(w * h); // rows=W x cols=H
-
-    // Прогружаем входные данные по PCI-E шине: CPU RAM -> GPU VRAM
+    gpu::gpu_mem_32f input_matrix_gpu (h * w);
+    gpu::gpu_mem_32f output_matrix_gpu(w * h);
     input_matrix_gpu.writeN(input_cpu.data(), h * w);
 
     std::vector<std::string> algorithm_names = {
@@ -63,45 +57,16 @@ void run(int argc, char** argv)
         std::cout << "______________________________________________________" << std::endl;
         std::cout << "Evaluating algorithm #" << (algorithm_index + 1) << "/" << algorithm_names.size() << ": " << algorithm << std::endl;
 
-        // Запускаем алгоритм (несколько раз и с замером времени выполнения)
         std::vector<double> times;
         for (int iter = 0; iter < 10; ++iter) {
             timer t;
 
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED); // TODO remove me
-            // _______________________________OpenCL_____________________________________________
-            if (context.type() == gpu::Context::TypeOpenCL) {
-                if (algorithm == "01 naive transpose (non-coalesced)") {
-                    ocl_matrix01TransposeNaive.exec(gpu::WorkSize(1, 1, w, h), input_matrix_gpu, output_matrix_gpu, w, h);
-                } else if (algorithm == "02 transpose via local memory (coalesced)") {
-                    ocl_matrix02TransposeCoalescedViaLocalMemory.exec(gpu::WorkSize(1, 1, w, h), input_matrix_gpu, output_matrix_gpu, w, h);
-                } else {
-                    rassert(false, 652345234321, algorithm, algorithm_index);
-                }
-                // _______________________________CUDA___________________________________________
-            } else if (context.type() == gpu::Context::TypeCUDA) {
-                if (algorithm == "01 naive transpose (non-coalesced)") {
-                    cuda::matrix_transpose_naive(gpu::WorkSize(GROUP_SIZE, 1, w, h), input_matrix_gpu, output_matrix_gpu, w, h);
-                } else if (algorithm == "02 transpose via local memory (coalesced)") {
-                    cuda::matrix_transpose_coalesced_via_local_memory(gpu::WorkSize(GROUP_SIZE_X, GROUP_SIZE_Y, w, h), input_matrix_gpu, output_matrix_gpu, w, h);
-                } else {
-                    rassert(false, 652345234321, algorithm, algorithm_index);
-                }
-                // _______________________________Vulkan_________________________________________
-            } else if (context.type() == gpu::Context::TypeVulkan) {
-                struct {
-                    unsigned int w;
-                    unsigned int h;
-                } params = {w, h};
-                if (algorithm == "01 naive transpose (non-coalesced)") {
-                    vk_matrix01TransposeNaive.exec(params, gpu::WorkSize(1, 1, w, h), input_matrix_gpu, output_matrix_gpu);
-                } else if (algorithm == "02 transpose via local memory (coalesced)") {
-                    vk_matrix02TransposeCoalescedViaLocalMemory.exec(params, gpu::WorkSize(1, 1, w, h), input_matrix_gpu, output_matrix_gpu);
-                } else {
-                    rassert(false, 652345234321, algorithm, algorithm_index);
-                }
+            if (algorithm == "01 naive transpose (non-coalesced)") {
+                ocl_matrix01TransposeNaive.exec(gpu::WorkSize(16, 16, w, h), input_matrix_gpu, output_matrix_gpu, w, h);
+            } else if (algorithm == "02 transpose via local memory (coalesced)") {
+                ocl_matrix02TransposeCoalescedViaLocalMemory.exec(gpu::WorkSize(16, 16, w, h), input_matrix_gpu, output_matrix_gpu, w, h);
             } else {
-                rassert(false, 546345243, context.type());
+                rassert(false, 652345234321, algorithm, algorithm_index);
             }
 
             times.push_back(t.elapsed());
