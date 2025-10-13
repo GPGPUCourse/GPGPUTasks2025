c0d5bbcea0c124690cd83903ef71174bb5eb771c
Mikhail Perveev <perveev_m@mail.ru>
2025-10-14 01:24:52 +0300

    Task 2 solved

--- src/kernels/cu/sum_04_local_reduction.cu
+++ src/kernels/cu/sum_04_local_reduction.cu
@@ -13,13 +13,25 @@ __global__ void sum_04_local_reduction(
     unsigned int* b,
     unsigned int  n)
 {
-    // Подсказки:
-    // const uint index = blockIdx.x * blockDim.x + threadIdx.x;
-    // const uint local_index = threadIdx.x;
-    // __shared__ unsigned int local_data[GROUP_SIZE];
-    // __syncthreads();
+    const unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int local_index = threadIdx.x;
 
-    // TODO
+    __shared__ unsigned int local_data[GROUP_SIZE];
+    if (index < n) {
+        local_data[local_index] = a[index];
+    } else {
+        local_data[local_index] = 0;
+    }
+
+    __syncthreads();
+
+    if (local_index == 0) {
+        unsigned int local_sum = 0;
+        for (unsigned int x : local_data) {
+            local_sum += x;
+        }
+        b[blockIdx.x] = local_sum;
+    }
 }
 
 namespace cuda {
