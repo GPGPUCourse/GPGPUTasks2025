80e732dd67dd9a0eefbcd465f4b452c5be861349
Artyom Korolyov <artemkorolev-2002@yandex.ru>
2025-10-14 23:30:16 +0300

    Solved task01

--- src/kernels/cu/aplusb_matrix_bad.cu
+++ src/kernels/cu/aplusb_matrix_bad.cu
@@ -10,15 +10,18 @@ __global__ void aplusb_matrix_bad(const unsigned int* a,
                        const unsigned int* b,
                              unsigned int* c,
                              unsigned int  width,
-                             unsigned int  height)
-{
+                             unsigned int  height) {
     // все три массива - линейно выложенные двумерные матрицы размера width (число столбиков) x height (число рядов)
     // при этом в памяти подряд идут элементы являющимися соседями в рамках одного ряда,
     // т.е. матрица выложена в памяти линейно ряд за рядом
     // т.е. если в матрице сделать шаг вправо или влево на одну ячейку - то в памяти мы шагнем на 4 байта
     // т.е. если в матрице сделать шаг вверх или вниз на одну ячейку - то в памяти мы шагнем на так называемый stride=width*4 байта
+    const unsigned int n = width * height;
 
-    // TODO реализуйте этот кернел - просуммируйте две матрицы так чтобы получить максимально ПЛОХУЮ производительность с точки зрения memory coalesced паттерна доступа
+    if (const unsigned int id = blockIdx.x * blockDim.x + threadIdx.x; id < n) {
+        const unsigned int i = n - id - 1;
+        c[i] = a[i] + b[i];
+    }
 }
 
 namespace cuda {
@@ -28,7 +31,10 @@ void aplusb_matrix_bad(const gpu::WorkSize &workSize,
     gpu::Context context;
     rassert(context.type() == gpu::Context::TypeCUDA, 34523543124312, context.type());
     cudaStream_t stream = context.cudaStream();
-    ::aplusb_matrix_bad<<<workSize.cuGridSize(), workSize.cuBlockSize(), 0, stream>>>(a.cuptr(), b.cuptr(), c.cuptr(), width, height);
+    unsigned int total = width * height;
+    dim3 blocks = workSize.cuGridSize();
+    dim3 threads = workSize.cuBlockSize();
+    ::aplusb_matrix_bad<<<blocks, threads, 0, stream>>>(a.cuptr(), b.cuptr(), c.cuptr(), width, height);
     CUDA_CHECK_KERNEL(stream);
 }
 } // namespace cuda
