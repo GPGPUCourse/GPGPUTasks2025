18da50be00ac56300cdb4d16a68078b46410c8bb
egfedoseev <eg.fedoseev2014@yandex.ru>
2025-10-17 04:16:15 +0300

    fix indexation

--- src/kernels/cl/prefix_sum_fused_compute_save_coalesced.cl
+++ src/kernels/cl/prefix_sum_fused_compute_save_coalesced.cl
@@ -8,10 +8,28 @@
 uint calcNextSum(__global const uint* pow2_sum,
                  const uint curN,
                  const uint x) {
-    if (x < (curN + 1) / 2) {
+    if (x * 2 < curN) {
+        // printf("curN: %d   sumIdx: %d   fst: %d   snd: %d\n",
+        //     curN, x, pow2_sum[x * 2], (x * 2 + 1 < curN ? pow2_sum[x * 2 + 1] : 0));
         return pow2_sum[x * 2] + (x * 2 + 1 < curN ? pow2_sum[x * 2 + 1] : 0);
     }
     return 0;
+}
+
+void writeNextPow(__global uint* next_pow2_sum,
+                const uint* sum,
+                const uint sumIdx,
+                const uint curN,
+                const uint x,
+                const uint newX,
+                const uint cntBlocks,
+                const uint offset) {
+    const uint idx = ((x < offset) ? x : newX / cntBlocks);
+    if (sumIdx + idx < (curN + 1) / 2 && idx < 2) {
+        // printf("x: %d\tidx: %d\tsumIdx: %d\tsum: %d\n",
+        //     x, idx, sumIdx, sum[idx]);
+        next_pow2_sum[sumIdx + idx] = sum[idx];
+    }
 }
 
 __attribute__((reqd_work_group_size(GROUP_SIZE, 1, 1)))
@@ -25,31 +43,52 @@ __kernel void prefix_sum_fused_compute_save_coalesced(
 {
     const uint x = get_global_id(0);
     const uint range = (1 << pow2);
-    const uint cntBlocks = (n + 1) / (range << 1);
-    if (x / cntBlocks >= range) {
-        return;
+    uint cntBlocks = (n + 1) / (range << 1);
+    uint offset = (n + 1) % (range << 1);
+    if (offset > range) {
+        offset -= range;
+        // ++cntBlocks;
+        // if (cntBlocks * range > (n + 1) / 2) {
+        //     printf("wtf\n");
+        // }
+    } else {
+        offset = 0;
     }
-    const uint blockNum = x % cntBlocks;
-    uint sumIdx = blockNum * 2;
-    const uint begIdx = sumIdx * range + range - 1;  
-    const uint idx = begIdx + (x / cntBlocks);  
 
-    uint sum0 = calcNextSum(pow2_sum, curN, sumIdx);
-    uint sum1 = calcNextSum(pow2_sum, curN, sumIdx + 1);
+    uint idx;
+    uint sumIdx;
+    uint newX = x - offset;
+    if (x < offset) {
+        idx = n - x - 1;
+        sumIdx = cntBlocks * 2;
+        // printf("pow2: %d   offset: %d   x: %d   idx: %d   sumIdx: %d\n",
+        //     pow2, offset, x, idx, sumIdx);
+    } else {
+        if (newX / cntBlocks >= range) {
+            return;
+        }
+        const uint blockNum = newX % cntBlocks;
+        sumIdx = blockNum * 2;
+        const uint begIdx = sumIdx * range + range - 1;  
+        idx = begIdx + (newX / cntBlocks);
+    }
+
+
+    uint sum[2] = {calcNextSum(pow2_sum, curN, sumIdx),
+        calcNextSum(pow2_sum, curN, sumIdx + 1)};
 
     if (idx < n) {
-        prefix_sum_accum[idx] += sum0;
+        prefix_sum_accum[idx] += sum[0];
+        // printf("pow2: %d   idx: %d   sum: %d   sumIdx: %d   res: %d\n", 
+        //     pow2, idx, sum[0], sumIdx, prefix_sum_accum[idx]);
     }
 
-    if (sumIdx < (curN + 1) / 2 && x / cntBlocks == 0) {
-        // printf("x: %d\tidx: %d\tpow2: %d\tsumIdx: %d\tsum0: %d\n",
-        //     x, idx, pow2, sumIdx, sum0);
-        next_pow2_sum[sumIdx] = sum0;
+    if (x == 0 && curN % 2 == 1) {
+        next_pow2_sum[curN / 2] = pow2_sum[curN - 1];
     }
-    ++sumIdx;
-    if (sumIdx < (curN + 1) / 2 && x / cntBlocks == 1) {
-        // printf("x: %d\tidx: %d\tpow2: %d\tsumIdx: %d\tsum1: %d\n",
-        //     x, idx, pow2, sumIdx, sum1);
-        next_pow2_sum[sumIdx] = sum1;
-    }
+
+
+
+    writeNextPow(next_pow2_sum, sum, sumIdx, curN, x, newX, cntBlocks, offset);
+    // writeNextPow(next_pow2_sum, sum, sumIdx + 1, curN, x, newX, cntBlocks);
 }
--- src/main_prefix_sum.cpp
+++ src/main_prefix_sum.cpp
@@ -46,7 +46,7 @@ void run(int argc, char** argv)
     avk2::KernelSource vk_prefix_accumulation(avk2::getPrefixSum02PrefixAccumulation());
 
     unsigned int n = 100*1000*1000;
-    // unsigned int n = 8;
+    // unsigned int n = 10;
     std::vector<unsigned int> as(n, 0);
     size_t total_sum = 0;
     for (size_t i = 0; i < n; ++i) {
@@ -56,11 +56,11 @@ void run(int argc, char** argv)
         total_sum += as[i];
         rassert(total_sum < std::numeric_limits<unsigned int>::max(), 5462345234231, total_sum, as[i], i); // ensure no overflow
     }
-    // std::cout << "515: " << as[515] << '\n';
+    // std::cout << '\n';
 
     // Аллоцируем буферы в VRAM
     gpu::gpu_mem_32u input_gpu(n), buffer1_pow2_sum_gpu(n), buffer2_pow2_sum_gpu(n), prefix_sum_accum_gpu(n);
-    gpu::gpu_mem_32u buffer3_even((n + 1) / 2);
+    // gpu::gpu_mem_32u buffer3_even((n + 1) / 2);
 
     // Прогружаем входные данные по PCI-E шине: CPU RAM -> GPU VRAM
     input_gpu.writeN(as.data(), n);
@@ -79,19 +79,28 @@ void run(int argc, char** argv)
         int curN = n;
         int pow2 = 0;
         // buffer1_pow2_sum_gpu.writeN(as.data(), n);
-        // ocl_sum_fused.exec(gpu::WorkSize(GROUP_SIZE, (n + 1) / 2), 
+        // ocl_sum_fused.exec(gpu::WorkSize(GROUP_SIZE, (n + 1) / 2),
         //     input_gpu, buffer1_pow2_sum_gpu, buffer3_even, prefix_sum_accum_gpu, curN, n, pow2);
         ocl_copy_array.exec(gpu::WorkSize(GROUP_SIZE, n), input_gpu, buffer1_pow2_sum_gpu, n);
         ocl_prefix_accumulation.exec(gpu::WorkSize(GROUP_SIZE, n), buffer1_pow2_sum_gpu, prefix_sum_accum_gpu, n, pow2);
-        while (curN > 1){
+        while (curN > 1) {
             ++pow2;
-            
+
             // std::cout << "curN: " << curN << "   pow2: " << pow2 << '\n';
-            ocl_sum_fused.exec(gpu::WorkSize(GROUP_SIZE, (n + 1) / 2), buffer1_pow2_sum_gpu, 
+            ocl_sum_fused.exec(gpu::WorkSize(GROUP_SIZE, (n + 1) / 2), buffer1_pow2_sum_gpu,
                 buffer2_pow2_sum_gpu, prefix_sum_accum_gpu, curN, n, pow2);
-            // ocl_sum_reduction.exec(gpu::WorkSize(GROUP_SIZE, (curN + 1) / 2), 
+            // ocl_sum_reduction.exec(gpu::WorkSize(GROUP_SIZE, (curN + 1) / 2),
             //     buffer1_pow2_sum_gpu, buffer2_pow2_sum_gpu, curN);
             curN = (curN + 1) / 2;
+
+            // std::vector<unsigned int> tmp(curN);
+            // buffer2_pow2_sum_gpu.readN(tmp.data(), curN);
+            // std::cout << "buffer " << pow2 << '\n';
+            // for (int i = 0; i < curN; ++i) {
+            //     std::cout << tmp[i] << ' ';
+            // }
+            // std::cout << "\n\n";
+
             // printf("\n");
             // ocl_prefix_accumulation.exec(gpu::WorkSize(GROUP_SIZE, n), buffer2_pow2_sum_gpu, prefix_sum_accum_gpu, n, pow2);
             std::swap(buffer1_pow2_sum_gpu, buffer2_pow2_sum_gpu);
@@ -115,6 +124,7 @@ void run(int argc, char** argv)
         rassert(cpu_sum == gpu_prefix_sum[i], 566324523452323, cpu_sum, gpu_prefix_sum[i], i);
     }
 
+    // std::cout << "n done: " << n << std::endl;
     // Проверяем что входные данные остались нетронуты (ведь мы их переиспользуем от итерации к итерации)
     std::vector<unsigned int> input_values = input_gpu.readVector();
     for (size_t i = 0; i < n; ++i) {
@@ -131,7 +141,8 @@ int main(int argc, char** argv)
         if (e.what() == DEVICE_NOT_SUPPORT_API) {
             // Возвращаем exit code = 0 чтобы на CI не было красного крестика о неуспешном запуске из-за выбора CUDA API (его нет на процессоре - т.е. в случае CI на GitHub Actions)
             return 0;
-        } if (e.what() == CODE_IS_NOT_IMPLEMENTED) {
+        }
+        if (e.what() == CODE_IS_NOT_IMPLEMENTED) {
             // Возвращаем exit code = 0 чтобы на CI не было красного крестика о неуспешном запуске из-за того что задание еще не выполнено
             return 0;
         } else {
