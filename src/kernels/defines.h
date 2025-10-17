df91d4864faa843562debf3d60fe6b47dd46edda
sprff <62142866+sprff@users.noreply.github.com>
2025-10-17 16:48:49 +0300

    Update defines.h

--- src/kernels/defines.h
+++ src/kernels/defines.h
@@ -1,7 +1,7 @@
 #ifndef my_defines_vk // pragma once
 #define my_defines_vk
 
-#define GROUP_SIZE 256
+#define GROUP_SIZE 512
 
 #define RASSERT_ENABLED 0 // disabled by default, enable for debug by changing 0 to 1, disable before performance evaluation/profiling/commiting
 
