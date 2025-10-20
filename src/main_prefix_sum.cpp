2d61e55df13159a8f27a33efac01a2db3645abcf
zdyaknoivl <itsoke@mail.ru>
2025-10-21 00:14:53 +0300

    task04 done

--- src/main_prefix_sum.cpp
+++ src/main_prefix_sum.cpp
@@ -65,10 +65,21 @@ void run(int argc, char** argv)
         // Если хотите - можете удалить ветвление здесь и оставить только тот код который соответствует вашему выбору API
         if (context.type() == gpu::Context::TypeOpenCL) {
             // TODO
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-            // ocl_fill_with_zeros.exec();
-            // ocl_sum_reduction.exec();
-            // ocl_prefix_accumulation.exec();
+            // ocl_fill_with_zeros.exec(gpu::WorkSize(256, n), buffer1_pow2_sum_gpu);
+            buffer1_pow2_sum_gpu.writeN(as.data(), n);
+            prefix_sum_accum_gpu.writeN(as.data(), n);
+
+            unsigned int curLen = n;
+            unsigned int pow2 = 0;
+
+            while (curLen > 1) {
+                ocl_prefix_accumulation.exec(gpu::WorkSize(256, n), buffer1_pow2_sum_gpu, prefix_sum_accum_gpu, n, pow2);
+                const unsigned int nextLen = (curLen + 1) / 2;
+                ocl_sum_reduction.exec(gpu::WorkSize(256, nextLen), buffer1_pow2_sum_gpu, buffer2_pow2_sum_gpu, curLen);
+                buffer1_pow2_sum_gpu.swap(buffer2_pow2_sum_gpu);
+                curLen = nextLen;
+                ++pow2;
+            }
         } else if (context.type() == gpu::Context::TypeCUDA) {
             // TODO
             throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
