fd4ca64001294ecfbf62cdd458020ef37bc83920
NovokshanovE <novokshanov.eugene@yandex.ru>
2025-10-27 21:18:06 +0300

    fix

--- src/kernels/defines.h
+++ src/kernels/defines.h
@@ -1,8 +1,8 @@
 #ifndef my_defines_vk // pragma once
 #define my_defines_vk
 
-#define GROUP_SIZE   512
-#define GROUP_SIZE_X 32
+#define GROUP_SIZE   256
+#define GROUP_SIZE_X 16
 #define GROUP_SIZE_Y 16
 
 #define RASSERT_ENABLED 0 // disabled by default, enable for debug by changing 0 to 1, disable before performance evaluation/profiling/commiting
