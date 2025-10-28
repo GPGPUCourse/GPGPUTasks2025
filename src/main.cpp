d69cb67cfa78f753ac3ae9d24befc732cd44163b
panda <pandmacs@gmail.com>
2025-10-28 16:02:43 +0300

    got all devices

--- src/main.cpp
+++ src/main.cpp
@@ -46,15 +46,13 @@ int main()
 	std::vector<cl_platform_id> platforms(platformsCount);
 	OCL_SAFE_CALL(clGetPlatformIDs(platformsCount, platforms.data(), nullptr));
 
-	for(int platformIndex = 0; platformIndex < platformsCount; ++platformIndex)
+	for(unsigned int platformIndex = 0; platformIndex < platformsCount; ++platformIndex)
 	{
 		std::cout << "Platform #" << (platformIndex + 1) << "/" << platformsCount << std::endl;
 		cl_platform_id platform = platforms[platformIndex];
 
 		// Откройте документацию по "OpenCL Runtime" -> "Query Platform Info" -> "clGetPlatformInfo"
 		// Не забывайте проверять коды ошибок с помощью макроса OCL_SAFE_CALL
-		size_t platformNameSize = 0;
-		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, 0, nullptr, &platformNameSize));
 		// TODO 1.1
 		// Попробуйте вместо CL_PLATFORM_NAME передать какое-нибудь случайное число - например 239
 		// Т.к. это некорректный идентификатор параметра платформы - то метод вернет код ошибки
@@ -67,20 +65,61 @@ int main()
 		// в документации подробно объясняется, какой ситуации соответствует данная ошибка, и это позволит, проверив код, понять, чем же вызвана данная ошибка (некорректным аргументом param_name)
 		// Обратите внимание, что в этом же libs/clew/CL/cl.h файле указаны всевоможные defines, такие как CL_DEVICE_TYPE_GPU и т.п.
 
+		// originally
+
+		size_t platformNameSize = 0;
+		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, 0, nullptr, &platformNameSize));
+
+		// При подстановке неверного числа получаем код ошибки -30, что в терминах OPENCL значит CL_INVALID_VALUE
+		// try
+		// {
+		// 	size_t platformNameSize = 0;
+		// 	OCL_SAFE_CALL(clGetPlatformInfo(platform, 239, 0, nullptr, &platformNameSize));
+		// 	std::cout << "ERROR: Should have thrown an exception but didn't!" << std::endl;
+		// }
+		// catch(const std::runtime_error &e)
+		// {
+		// 	std::cout << "Successfully caught error: " << e.what() << std::endl;
+
+		// 	// Проверим код ошибки более детально
+		// 	cl_int directError = clGetPlatformInfo(platform, 239, 0, nullptr, &platformNameSize);
+		// 	std::cout << "Direct error code: " << directError << std::endl;
+
+		// 	// Теперь получим информацию с корректным параметром
+		// 	OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, 0, nullptr, &platformNameSize));
+		// }
+
 		// TODO 1.2
 		// Аналогично тому, как был запрошен список идентификаторов всех платформ - так и с названием платформы, теперь, когда известна длина названия - его можно запросить:
 		std::vector<unsigned char> platformName(platformNameSize, 0);
-		// clGetPlatformInfo(...);
+		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, platformNameSize, platformName.data(), nullptr));
+
 		std::cout << "    Platform name: " << platformName.data() << std::endl;
 
 		// TODO 1.3
 		// Запросите и напечатайте так же в консоль вендора данной платформы
 
+		size_t platfromVendorSize = 0;
+		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, 0, nullptr, &platfromVendorSize));
+
+		std::vector<unsigned char> platformVendor(platfromVendorSize);
+		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, platfromVendorSize, platformVendor.data(), nullptr));
+
+		std::cout << "    Vendor name: " << platformVendor.data() << std::endl;
 		// TODO 2.1
 		// Запросите число доступных устройств данной платформы (аналогично тому, как это было сделано для запроса числа доступных платформ - см. секцию "OpenCL Runtime" -> "Query Devices")
 		cl_uint devicesCount = 0;
+		OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, nullptr, &devicesCount));
 
-		for(int deviceIndex = 0; deviceIndex < devicesCount; ++deviceIndex)
+		std::cout << "    Devices count: " << devicesCount << std::endl;
+
+		if(devicesCount == 0)
+			continue;
+
+		std::vector<cl_device_id> devices(devicesCount);
+		OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, devicesCount, devices.data(), nullptr));
+
+		for(unsigned int deviceIndex = 0; deviceIndex < devicesCount; ++deviceIndex)
 		{
 			// TODO 2.2
 			// Запросите и напечатайте в консоль:
@@ -88,6 +127,46 @@ int main()
 			// - Тип устройства (видеокарта/процессор/что-то странное)
 			// - Размер памяти устройства в мегабайтах
 			// - Еще пару или более свойств устройства, которые вам покажутся наиболее интересными
+			cl_device_id device = devices[deviceIndex];
+			std::cout << "    Device " << (deviceIndex + 1) << '/' << devicesCount << std::endl;
+
+			// Name
+			size_t deviceNameSize = 0;
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_NAME, 0, nullptr, &deviceNameSize));
+			std::vector<unsigned char> deviceName(deviceNameSize, 0);
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_NAME, deviceNameSize, deviceName.data(), nullptr));
+			std::cout << "        Name: " << deviceName.data() << std::endl;
+
+			// Type (GPU/CPU/else)
+			cl_device_type deviceType;
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_TYPE, sizeof(deviceType), &deviceType, nullptr));
+
+			std::string deviceTypeStr;
+			if(deviceType & CL_DEVICE_TYPE_CPU)
+				deviceTypeStr = "CPU";
+			else if(deviceType & CL_DEVICE_TYPE_GPU)
+				deviceTypeStr = "GPU";
+			else if(deviceType & CL_DEVICE_TYPE_ACCELERATOR)
+				deviceTypeStr = "Accelerator";
+			else
+				deviceTypeStr = "Unknown";
+
+			std::cout << "        Type: " << deviceTypeStr << std::endl;
+
+			// Memory size
+			cl_ulong memSize = 0;
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(memSize), &memSize, nullptr));
+			std::cout << "        Global memory: " << (memSize / (1024 * 1024)) << " MB" << std::endl;
+
+			// clockFrequency
+			cl_uint clockFreq = 0;
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(clockFreq), &clockFreq, nullptr));
+			std::cout << "        Max clock frequency: " << clockFreq << " MHz" << std::endl;
+
+			// max workgroup size
+			size_t maxWorkGroupSize = 0;
+			OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(maxWorkGroupSize), &maxWorkGroupSize, nullptr));
+			std::cout << "        Max work group size: " << maxWorkGroupSize << std::endl;
 		}
 	}
 
