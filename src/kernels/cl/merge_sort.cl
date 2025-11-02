9c614a8a34b6bbc50604bfd784e81f2c32cd058d
Cifoviko <cifoviko@gmail.com>
2025-11-02 23:00:14 +0300

    done

--- src/kernels/cl/merge_sort.cl
+++ src/kernels/cl/merge_sort.cl
@@ -5,13 +5,57 @@
 #include "helpers/rassert.cl"
 #include "../defines.h"
 
-__attribute__((reqd_work_group_size(1, 1, 1)))
+static inline int bin_search(__global const uint* data, int start, int len, uint val, bool strong_cmp)
+{
+    int l = 0;
+    int r = len;
+
+    while (l < r) {
+        int m = (l + r) / 2;
+        if ((strong_cmp && (data[start + m] < val))
+            || (!strong_cmp && (data[start + m] <= val))) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+    }
+    return l;
+}
+
+__attribute__((reqd_work_group_size(GROUP_SIZE, 1, 1)))
 __kernel void merge_sort(
     __global const uint* input_data,
     __global       uint* output_data,
                    int  sorted_k,
                    int  n)
 {
-    const unsigned int i = get_global_id(0);
-    // TODO
+    // DONE
+
+    const int global_id = get_global_id(0);
+
+    if (global_id >= n) {
+        return;
+    }
+
+    const int block_size = 2 * sorted_k;
+    const int block_id = global_id / block_size;
+    const int block_start = block_id * block_size;
+
+    const int left_start = block_start;
+    const int left_size = max(0, min(sorted_k, n - left_start));
+    const int right_start = left_start + sorted_k;
+    const int right_size = max(0, min(sorted_k, n - right_start));
+
+    const bool in_right = (global_id - block_start >= left_size);
+    const uint value = input_data[global_id];
+
+    int my_start = in_right ? right_start : left_start;
+    int other_start = in_right ? left_start : right_start;
+    int other_size = in_right ? left_size : right_size;
+
+    int out_position = left_start
+        + (global_id - my_start)
+        + bin_search(input_data, other_start, other_size, value, !in_right);
+
+    output_data[out_position] = value;
 }
--- src/main_merge_sort.cpp
+++ src/main_merge_sort.cpp
@@ -21,11 +21,11 @@ void run(int argc, char** argv)
     //   - Если аргумент запуска есть и он от 0 до N-1 - вернет устройство под указанным номером
     gpu::Device device = gpu::chooseGPUDevice(gpu::selectAllDevices(ALL_GPUS, true), argc, argv);
 
-    // TODO 000 сделайте здесь свой выбор API - если он отличается от OpenCL то в этой строке нужно заменить TypeOpenCL на TypeCUDA или TypeVulkan
-    // TODO 000 после этого изучите этот код, запустите его, изучите соответсвующий вашему выбору кернел - src/kernels/<ваш выбор>/aplusb.<ваш выбор>
-    // TODO 000 P.S. если вы выбрали CUDA - не забудьте установить CUDA SDK и добавить -DCUDA_SUPPORT=ON в CMake options
-    // TODO 010 P.S. так же в случае CUDA - добавьте в CMake options (НЕ меняйте сами CMakeLists.txt чтобы не менять окружение тестирования):
-    // TODO 010 "-DCMAKE_CUDA_ARCHITECTURES=75 -DCMAKE_CUDA_FLAGS=-lineinfo" (первое - чтобы включить поддержку WMMA, второе - чтобы compute-sanitizer и профилировщик знали номера строк кернела)
+    // DONE 000 сделайте здесь свой выбор API - если он отличается от OpenCL то в этой строке нужно заменить TypeOpenCL на TypeCUDA или TypeVulkan
+    // DONE 000 после этого изучите этот код, запустите его, изучите соответсвующий вашему выбору кернел - src/kernels/cl/aplusb.cl
+    // DONE 000 P.S. если вы выбрали CUDA - не забудьте установить CUDA SDK и добавить -DCUDA_SUPPORT=ON в CMake options
+    // DONE 010 P.S. так же в случае CUDA - добавьте в CMake options (НЕ меняйте сами CMakeLists.txt чтобы не менять окружение тестирования):
+    // DONE 010 "-DCMAKE_CUDA_ARCHITECTURES=75 -DCMAKE_CUDA_FLAGS=-lineinfo" (первое - чтобы включить поддержку WMMA, второе - чтобы compute-sanitizer и профилировщик знали номера строк кернела)
     gpu::Context context = activateContext(device, gpu::Context::TypeOpenCL);
     // OpenCL - рекомендуется как вариант по умолчанию, можно выполнять на CPU, есть printf, есть аналог valgrind/cuda-memcheck - https://github.com/jrprice/Oclgrind
     // CUDA   - рекомендуется если у вас NVIDIA видеокарта, есть printf, т.к. в таком случае вы сможете пользоваться профилировщиком (nsight-compute) и санитайзером (compute-sanitizer, это бывший cuda-memcheck)
@@ -40,9 +40,9 @@ void run(int argc, char** argv)
 
     FastRandom r;
 
-    int n = 100*1000*1000; // TODO при отладке используйте минимальное n (например n=5 или n=10) при котором воспроизводится бага
+    int n = 100*1000*1000; // DONE при отладке используйте минимальное n (например n=5 или n=10) при котором воспроизводится бага
     int min_value = 1; // это сделано для упрощения, чтобы существовало очевидное -INFINITY значение
-    int max_value = std::numeric_limits<int>::max() - 1; // TODO при отладке используйте минимальное max_value (например max_value=8) при котором воспроизводится бага
+    int max_value = std::numeric_limits<int>::max() - 1; // DONE при отладке используйте минимальное max_value (например max_value=8) при котором воспроизводится бага
     std::vector<unsigned int> as(n, 0);
     std::vector<unsigned int> sorted(n, 0);
     for (size_t i = 0; i < n; ++i) {
@@ -78,7 +78,7 @@ void run(int argc, char** argv)
 
     // Аллоцируем буферы в VRAM
     gpu::gpu_mem_32u input_gpu(n);
-    gpu::gpu_mem_32u buffer1_gpu(n), buffer2_gpu(n); // TODO это просто шаблонка, можете переименовать эти буферы, сделать другого размера/типа, удалить часть, добавить новые
+    gpu::gpu_mem_32u buffer1_gpu(n), buffer2_gpu(n); // DONE это просто шаблонка, можете переименовать эти буферы, сделать другого размера/типа, удалить часть, добавить новые
     gpu::gpu_mem_32u buffer_output_gpu(n);
 
     // Прогружаем входные данные по PCI-E шине: CPU RAM -> GPU VRAM
@@ -92,19 +92,34 @@ void run(int argc, char** argv)
 
     // Запускаем кернел (несколько раз и с замером времени выполнения)
     std::vector<double> times;
-    for (int iter = 0; iter < 10; ++iter) { // TODO при отладке запускайте одну итерацию
+    for (int iter = 0; iter < 10; ++iter) { // DONE при отладке запускайте одну итерацию
         timer t;
 
         // Запускаем кернел, с указанием размера рабочего пространства и передачей всех аргументов
         // Если хотите - можете удалить ветвление здесь и оставить только тот код который соответствует вашему выбору API
         if (context.type() == gpu::Context::TypeOpenCL) {
-            // TODO
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
+            // DONE
+
+            buffer1_gpu.writeN(as.data(), n);
+            gpu::gpu_mem_32u& buffer_in = buffer1_gpu;
+            gpu::gpu_mem_32u& buffer_out = buffer2_gpu;
+
+            int sorted_k = 1;
+            while (sorted_k < n) {
+                const size_t work_size = ((size_t)n + GROUP_SIZE - 1) / GROUP_SIZE * GROUP_SIZE;
+                ocl_mergeSort.exec(gpu::WorkSize(GROUP_SIZE, work_size), buffer_in, buffer_out, sorted_k, n);
+
+                sorted_k *= 2;
+                std::swap(buffer_in, buffer_out);
+            }
+
+            buffer_in.copyToN(buffer_output_gpu, n);
+
         } else if (context.type() == gpu::Context::TypeCUDA) {
-            // TODO
+            // SKIP
             throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
         } else if (context.type() == gpu::Context::TypeVulkan) {
-            // TODO
+            // SKIP
             throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
         } else {
             rassert(false, 4531412341, context.type());
