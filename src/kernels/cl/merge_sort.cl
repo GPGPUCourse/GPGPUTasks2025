05aa633dc6a46a2c3d6a620795eb94e233bfeac2
hrundelair <andrew.ratkov30@gmail.com>
2025-11-02 14:38:42 +0300

    mergesort hw done

--- src/kernels/cl/merge_sort.cl
+++ src/kernels/cl/merge_sort.cl
@@ -1,17 +1,88 @@
-#ifdef __CLION_IDE__
-#include <libgpu/opencl/cl/clion_defines.cl> // This file helps CLion IDE to know what additional functions exists in OpenCL's extended C99
-#endif
-
 #include "helpers/rassert.cl"
 #include "../defines.h"
 
-__attribute__((reqd_work_group_size(1, 1, 1)))
+
+__attribute__((reqd_work_group_size(GROUP_SIZE, 1, 1)))
 __kernel void merge_sort(
-    __global const uint* input_data,
-    __global       uint* output_data,
-                   int  sorted_k,
-                   int  n)
+    __global const uint* restrict input_data,
+    __global       uint* restrict output_data,
+    const int sorted_now_log,
+    const int n)
 {
-    const unsigned int i = get_global_id(0);
-    // TODO
+    __local unsigned int local_data[GROUP_SIZE * DEFAULT_SORT_BLOCK];
+    const unsigned int idx = get_global_id(0);
+    const unsigned int local_idx = idx & 255u;
+    const unsigned int local_base = local_idx << 2;
+    unsigned int temp;
+
+    if (sorted_now_log == 0) {
+        for (unsigned int j = 0u; j < DEFAULT_SORT_BLOCK; ++j) {
+            local_data[local_base + j] = (((idx << 2) + j < n) ? input_data[(idx << 2) + j] : UINT_MAX);
+        }        
+
+        if (local_data[local_base] > local_data[local_base + 1]) {
+            temp = local_data[local_base];
+            local_data[local_base] = local_data[local_base + 1];
+            local_data[local_base + 1] = temp;
+        }
+
+        if (local_data[local_base + 2] > local_data[local_base + 3]) {
+            temp = local_data[local_base + 2];
+            local_data[local_base + 2] = local_data[local_base + 3];
+            local_data[local_base + 3] = temp;
+        }
+
+        if (local_data[local_base + 1] > local_data[local_base + 3]) {
+            temp = local_data[local_base + 1];
+            local_data[local_base + 1] = local_data[local_base + 3];
+            local_data[local_base + 3] = temp;
+        }
+
+        if (local_data[local_base] > local_data[local_base + 2]) {
+            temp = local_data[local_base];
+            local_data[local_base] = local_data[local_base + 2];
+            local_data[local_base + 2] = temp;
+        }
+
+        if (local_data[local_base + 1] > local_data[local_base + 2]) {
+            temp = local_data[local_base + 1];
+            local_data[local_base + 1] = local_data[local_base + 2];
+            local_data[local_base + 2] = temp;
+        }
+
+        for (unsigned int j = 0u; j < DEFAULT_SORT_BLOCK; ++j) {
+            if ((idx << 2) + j < n) {
+                output_data[(idx << 2) + j] = local_data[local_base + j];
+            }
+        }
+    } else {
+        const unsigned int val = ((idx < n) ? input_data[idx] : 0xfffffffdu);
+        const int defaultL = ((idx >> sorted_now_log) ^ 1u) << sorted_now_log;
+        const unsigned int searching_in_left_half = (defaultL <= idx);
+        const unsigned int comp_val = (searching_in_left_half ? val + 1u : val);
+
+        int L = defaultL - 1;
+        int R = defaultL + (1 << sorted_now_log);
+
+
+        while (R - L > 1) {
+            const unsigned int M = L + R >> 1;
+            const unsigned int comparing_to = (M < n ? input_data[M] : UINT_MAX);
+            if (comparing_to >= comp_val) {
+                R = M;
+            } else {
+                L = M;
+            }
+        }
+
+
+        int idx_to_put = idx + R - defaultL;
+        if (searching_in_left_half) idx_to_put -= (1 << sorted_now_log);
+
+        // if (idx < 64) {
+        //     printf("idx=%d, L=%d, R=%d, searching_in_left_half=%d, comp_val=%d, idx_to_put=%d\n", idx, L, R, searching_in_left_half, comp_val, idx_to_put);
+        // }
+
+        if (idx_to_put < n) output_data[idx_to_put] = val;
+    }
 }
--- src/main_merge_sort.cpp
+++ src/main_merge_sort.cpp
@@ -11,6 +11,26 @@
 
 #include <fstream>
 
+
+#define DEBUG 0
+
+#if DEBUG
+#define ITERS 1
+
+// for debug only
+void print_gpu_vector(const gpu::gpu_mem_32u& buf) {
+    const std::vector<unsigned int> vec = buf.readVector();
+    for (size_t i = 0; i < vec.size(); ++i) {
+        std::cout << "(" << i << ": " <<  vec[i] << ") ";
+        if (i % 16 == 15) std::cout << '\n';
+    }
+    std::cout << std::endl;
+}
+
+#else
+#define ITERS 10
+#endif
+
 void run(int argc, char** argv)
 {
     // chooseGPUVkDevices:
@@ -20,31 +40,23 @@ void run(int argc, char** argv)
     //   - Если аргументов запуска нет или переданное число не находится в диапазоне от 0 до N-1 - кинет ошибку
     //   - Если аргумент запуска есть и он от 0 до N-1 - вернет устройство под указанным номером
     gpu::Device device = gpu::chooseGPUDevice(gpu::selectAllDevices(ALL_GPUS, true), argc, argv);
-
-    // TODO 000 сделайте здесь свой выбор API - если он отличается от OpenCL то в этой строке нужно заменить TypeOpenCL на TypeCUDA или TypeVulkan
-    // TODO 000 после этого изучите этот код, запустите его, изучите соответсвующий вашему выбору кернел - src/kernels/<ваш выбор>/aplusb.<ваш выбор>
-    // TODO 000 P.S. если вы выбрали CUDA - не забудьте установить CUDA SDK и добавить -DCUDA_SUPPORT=ON в CMake options
-    // TODO 010 P.S. так же в случае CUDA - добавьте в CMake options (НЕ меняйте сами CMakeLists.txt чтобы не менять окружение тестирования):
-    // TODO 010 "-DCMAKE_CUDA_ARCHITECTURES=75 -DCMAKE_CUDA_FLAGS=-lineinfo" (первое - чтобы включить поддержку WMMA, второе - чтобы compute-sanitizer и профилировщик знали номера строк кернела)
     gpu::Context context = activateContext(device, gpu::Context::TypeOpenCL);
-    // OpenCL - рекомендуется как вариант по умолчанию, можно выполнять на CPU, есть printf, есть аналог valgrind/cuda-memcheck - https://github.com/jrprice/Oclgrind
-    // CUDA   - рекомендуется если у вас NVIDIA видеокарта, есть printf, т.к. в таком случае вы сможете пользоваться профилировщиком (nsight-compute) и санитайзером (compute-sanitizer, это бывший cuda-memcheck)
-    // Vulkan - не рекомендуется, т.к. писать код (compute shaders) на шейдерном языке GLSL на мой взгляд менее приятно чем в случае OpenCL/CUDA
-    //          если же вас это не останавливает - профилировщик (nsight-systems) при запуске на NVIDIA тоже работает (хоть и менее мощный чем nsight-compute)
-    //          кроме того есть debugPrintfEXT(...) для вывода в консоль с видеокарты
-    //          кроме того используемая библиотека поддерживает rassert-проверки (своеобразные инварианты с уникальным числом) на видеокарте для Vulkan
 
     ocl::KernelSource ocl_mergeSort(ocl::getMergeSort());
 
-    avk2::KernelSource vk_mergeSort(avk2::getMergeSort());
-
     FastRandom r;
 
-    int n = 100*1000*1000; // TODO при отладке используйте минимальное n (например n=5 или n=10) при котором воспроизводится бага
-    int min_value = 1; // это сделано для упрощения, чтобы существовало очевидное -INFINITY значение
-    int max_value = std::numeric_limits<int>::max() - 1; // TODO при отладке используйте минимальное max_value (например max_value=8) при котором воспроизводится бага
+#if DEBUG
+    const int n = 1000;
+#else
+    const int n = 100*1000*1000;
+#endif
+
+    const int min_value = 1; // это сделано для упрощения, чтобы существовало очевидное -INFINITY значение
+    const int max_value = std::numeric_limits<int>::max() - 1; // TODO при отладке используйте минимальное max_value (например max_value=8) при котором воспроизводится бага
     std::vector<unsigned int> as(n, 0);
     std::vector<unsigned int> sorted(n, 0);
+
     for (size_t i = 0; i < n; ++i) {
         as[i] = r.next(min_value, max_value);
     }
@@ -71,43 +83,36 @@ void run(int argc, char** argv)
         timer t;
         std::sort(sorted.begin(), sorted.end());
         // Вычисляем достигнутую эффективную пропускную способность видеопамяти (из соображений что мы отработали в один проход - считали массив и сохранили его переупорядоченным)
-        double memory_size_gb = sizeof(unsigned int) * 2 * n / 1024.0 / 1024.0 / 1024.0;
+        const double memory_size_gb = sizeof(unsigned int) * 2 * n / 1024.0 / 1024.0 / 1024.0;
         std::cout << "CPU std::sort finished in " << t.elapsed() << " sec" << std::endl;
         std::cout << "CPU std::sort effective RAM bandwidth: " << memory_size_gb / t.elapsed() << " GB/s (" << n / 1000 / 1000 / t.elapsed() << " uint millions/s)" << std::endl;
     }
 
     // Аллоцируем буферы в VRAM
     gpu::gpu_mem_32u input_gpu(n);
-    gpu::gpu_mem_32u buffer1_gpu(n), buffer2_gpu(n); // TODO это просто шаблонка, можете переименовать эти буферы, сделать другого размера/типа, удалить часть, добавить новые
+    gpu::gpu_mem_32u buffer1_gpu(n), buffer2_gpu(n);
     gpu::gpu_mem_32u buffer_output_gpu(n);
 
     // Прогружаем входные данные по PCI-E шине: CPU RAM -> GPU VRAM
     input_gpu.writeN(as.data(), n);
-    // Советую занулить (или еще лучше - заполнить какой-то уникальной константой, например 255) все буферы
-    // В некоторых случаях это ускоряет отладку, но обратите внимание, что fill реализован через копию множества нулей по PCI-E, то есть он очень медленный
-    // Если вам нужно занулять буферы в процессе вычислений - используйте кернел который это сделает (см. кернел fill_buffer_with_zeros)
-    buffer1_gpu.fill(255);
-    buffer2_gpu.fill(255);
-    buffer_output_gpu.fill(255);
 
     // Запускаем кернел (несколько раз и с замером времени выполнения)
     std::vector<double> times;
-    for (int iter = 0; iter < 10; ++iter) { // TODO при отладке запускайте одну итерацию
+    for (int iter = 0; iter < ITERS; ++iter) {
         timer t;
 
-        // Запускаем кернел, с указанием размера рабочего пространства и передачей всех аргументов
-        // Если хотите - можете удалить ветвление здесь и оставить только тот код который соответствует вашему выбору API
-        if (context.type() == gpu::Context::TypeOpenCL) {
-            // TODO
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-        } else if (context.type() == gpu::Context::TypeCUDA) {
-            // TODO
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-        } else if (context.type() == gpu::Context::TypeVulkan) {
-            // TODO
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-        } else {
+        if (context.type() != gpu::Context::TypeOpenCL) {
             rassert(false, 4531412341, context.type());
+        }
+
+        ocl_mergeSort.exec(gpu::WorkSize(GROUP_SIZE, (n + 3) >> 2), input_gpu, buffer1_gpu, 0, n);
+        
+        int cur_sorted_log = 2;
+        while ((1 << cur_sorted_log) < n) {
+            gpu::gpu_mem_32u& inp = ((cur_sorted_log & 1) ? buffer2_gpu : buffer1_gpu);
+            gpu::gpu_mem_32u& out = ((1 << (cur_sorted_log + 1)) >= n ? buffer_output_gpu : ((cur_sorted_log & 1) ? buffer1_gpu : buffer2_gpu));
+            ocl_mergeSort.exec(gpu::WorkSize(GROUP_SIZE, n), inp, out, cur_sorted_log, n);
+            ++cur_sorted_log;
         }
 
         times.push_back(t.elapsed());
@@ -115,11 +120,11 @@ void run(int argc, char** argv)
     std::cout << "GPU merge-sort times (in seconds) - " << stats::valuesStatsLine(times) << std::endl;
 
     // Вычисляем достигнутую эффективную пропускную способность видеопамяти (из соображений что мы отработали в один проход - считали массив и сохранили его переупорядоченным)
-    double memory_size_gb = sizeof(unsigned int) * 2 * n / 1024.0 / 1024.0 / 1024.0;
+    const double memory_size_gb = sizeof(unsigned int) * 2 * n / 1024.0 / 1024.0 / 1024.0;
     std::cout << "GPU merge-sort median effective VRAM bandwidth: " << memory_size_gb / stats::median(times) << " GB/s (" << n / 1000 / 1000 / stats::median(times) << " uint millions/s)" << std::endl;
 
     // Считываем результат по PCI-E шине: GPU VRAM -> CPU RAM
-    std::vector<unsigned int> gpu_sorted = buffer_output_gpu.readVector();
+    const std::vector<unsigned int> gpu_sorted = buffer_output_gpu.readVector();
 
     // Сверяем результат
     for (size_t i = 0; i < n; ++i) {
@@ -127,7 +132,7 @@ void run(int argc, char** argv)
     }
 
     // Проверяем что входные данные остались нетронуты (ведь мы их переиспользуем от итерации к итерации)
-    std::vector<unsigned int> input_values = input_gpu.readVector();
+    const std::vector<unsigned int> input_values = input_gpu.readVector();
     for (size_t i = 0; i < n; ++i) {
         rassert(input_values[i] == as[i], 6573452432, input_values[i], as[i]);
     }
