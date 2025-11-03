c4bc01216d498b5237b00a88a4286e73aed00046
Aleksei Tokarev <nonezonyx@gmail.com>
2025-11-03 23:30:48 +0300

    task06

--- src/kernels/cl/merge_sort.cl
+++ src/kernels/cl/merge_sort.cl
@@ -5,13 +5,48 @@
 #include "helpers/rassert.cl"
 #include "../defines.h"
 
-__attribute__((reqd_work_group_size(1, 1, 1)))
+__attribute__((reqd_work_group_size(GROUP_SIZE, 1, 1)))
 __kernel void merge_sort(
     __global const uint* input_data,
     __global       uint* output_data,
-                   int  sorted_k,
-                   int  n)
+                   uint  sorted_k,
+                   uint  n)
 {
     const unsigned int i = get_global_id(0);
-    // TODO
+
+    if (i >= n) {
+        return;
+    }
+
+    const uint size = 1 << sorted_k;
+    const uint block_i = i >> sorted_k;
+    const uint pair_start = i & ~((1 << (sorted_k + 1)) - 1);
+    if (pair_start + size >= n) {
+        output_data[i] = input_data[i];
+        return;
+    }
+
+    const uint bs_offset = pair_start + (1 - (block_i & 1)) * size;
+    uint opp_block_length;
+    if (block_i & 1) {
+        opp_block_length = size;
+    } else {
+        opp_block_length = min(size, n - (pair_start + size));
+    }
+
+    const uint value = input_data[i] + (block_i & 1);
+    uint low = 0;
+    uint high = opp_block_length;
+    while (low < high) {
+        uint mid = (low + high) / 2;
+        if (input_data[bs_offset + mid] < value) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    const uint count = low;
+
+    const uint index_in_block = i - (block_i << sorted_k);
+    output_data[pair_start + index_in_block + count] = input_data[i];
 }
--- src/main_merge_sort.cpp
+++ src/main_merge_sort.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <libbase/stats.h>
 #include <libutils/misc.h>
 
@@ -86,28 +87,17 @@ void run(int argc, char** argv)
     // Советую занулить (или еще лучше - заполнить какой-то уникальной константой, например 255) все буферы
     // В некоторых случаях это ускоряет отладку, но обратите внимание, что fill реализован через копию множества нулей по PCI-E, то есть он очень медленный
     // Если вам нужно занулять буферы в процессе вычислений - используйте кернел который это сделает (см. кернел fill_buffer_with_zeros)
+    buffer_output_gpu.writeN(as.data(), n);
     buffer1_gpu.fill(255);
-    buffer2_gpu.fill(255);
-    buffer_output_gpu.fill(255);
 
     // Запускаем кернел (несколько раз и с замером времени выполнения)
     std::vector<double> times;
     for (int iter = 0; iter < 10; ++iter) { // TODO при отладке запускайте одну итерацию
         timer t;
 
-        // Запускаем кернел, с указанием размера рабочего пространства и передачей всех аргументов
-        // Если хотите - можете удалить ветвление здесь и оставить только тот код который соответствует вашему выбору API
-        if (context.type() == gpu::Context::TypeOpenCL) {
-            // TODO
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-        } else if (context.type() == gpu::Context::TypeCUDA) {
-            // TODO
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-        } else if (context.type() == gpu::Context::TypeVulkan) {
-            // TODO
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-        } else {
-            rassert(false, 4531412341, context.type());
+        for (int sorted_k = 0; (1 << sorted_k) < n; ++sorted_k) {
+            ocl_mergeSort.exec(gpu::WorkSize(GROUP_SIZE, n), buffer_output_gpu, buffer1_gpu, sorted_k, n);
+            buffer_output_gpu.swap(buffer1_gpu);
         }
 
         times.push_back(t.elapsed());
