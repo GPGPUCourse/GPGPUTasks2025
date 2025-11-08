88bba35620d357e8216a75d5e2ff9a88348dce03
k0oshara <mi9992828@gmail.com>
2025-11-08 22:18:42 +0300

    add task07

--- src/main_csr_spmv.cpp
+++ src/main_csr_spmv.cpp
@@ -8,6 +8,7 @@
 
 #include "kernels/defines.h"
 #include "kernels/kernels.h"
+#include "libgpu/work_size.h"
 
 #include <fstream>
 
@@ -164,7 +165,8 @@ void run(int argc, char** argv)
             // Если хотите - можете удалить ветвление здесь и оставить только тот код который соответствует вашему выбору API
             if (context.type() == gpu::Context::TypeOpenCL) {
                 // TODO
-                throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
+                // throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
+                ocl_spvm.exec(gpu::WorkSize(GROUP_SIZE, nrows), csr_row_offsets_gpu, csr_columns_gpu, csr_values_gpu, vector_values_gpu, output_vector_values_gpu, nrows);
             } else if (context.type() == gpu::Context::TypeCUDA) {
                 // TODO
                 throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
