3b9601d69762127c25c0436e3dab959854e706f7
hac4erman <9613983333@mail.ru>
2025-11-29 23:30:04 +0300

    Disable rassert

--- src/kernels/defines.h
+++ src/kernels/defines.h
@@ -12,6 +12,6 @@
 
 #define MAX_STACK_SIZE 64
 
-#define RASSERT_ENABLED 1 // disabled by default, enable for debug by changing 0 to 1, disable before performance evaluation/profiling/commiting
+#define RASSERT_ENABLED 0 // disabled by default, enable for debug by changing 0 to 1, disable before performance evaluation/profiling/commiting
 
 #endif // pragma once
