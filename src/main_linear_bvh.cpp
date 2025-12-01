3ffe3c2903a55a29dc9a46e359ebf7dd05510b95
Gnihton <erik.yapparow@yandex.ru>
2025-12-02 00:01:54 +0300

    Fix

--- src/main_linear_bvh.cpp
+++ src/main_linear_bvh.cpp
@@ -94,7 +94,7 @@ void run(int argc, char** argv)
         "data/san-miguel/san-miguel.obj",
     };
 
-    const int niters = 10; // при отладке удобно запускать одну итерацию
+    const int niters = 5; // при отладке удобно запускать одну итерацию
     std::vector<double> gpu_rt_perf_mrays_per_sec;
     std::vector<double> gpu_lbvh_perfs_mtris_per_sec;
 
