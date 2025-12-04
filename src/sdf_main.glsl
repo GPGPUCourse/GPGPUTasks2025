e23f3ff7e14e37b1b34411dfe4491363bd8d3dc0
Amir Galeev <aagaleev_1@edu.hse.ru>
2025-12-04 22:22:20 +0300

    solve task09

--- src/sdf_main.glsl
+++ src/sdf_main.glsl
@@ -24,25 +24,72 @@ float lazycos(float angle)
     return 1.0;
 }
 
+float sdRoundCone( vec3 p, float r1, float r2, float h )
+{
+  float b = (r1-r2)/h;
+  float a = sqrt(1.0-b*b);
+
+  vec2 q = vec2( length(p.xz), p.y );
+  float k = dot(q,vec2(-b,a));
+  if( k<0.0 ) return length(q) - r1;
+  if( k>a*h ) return length(q-vec2(0.0,h)) - r2;
+  return dot(q, vec2(a,b) ) - r1;
+}
+
+float sdVerticalCapsule( vec3 p, float h, float r )
+{
+  p.y -= clamp( p.y, 0.0, h );
+  return length( p ) - r;
+}
+
+float sdCapsule( vec3 p, vec3 a, vec3 b, float r )
+{
+  vec3 pa = p - a, ba = b - a;
+  float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+  return length( pa - ba*h ) - r;
+}
+
 // возможно, для конструирования тела пригодятся какие-то примитивы из набора https://iquilezles.org/articles/distfunctions/
 // способ сделать гладкий переход между примитивами: https://iquilezles.org/articles/smin/
 vec4 sdBody(vec3 p)
 {
     float d = 1e10;
 
-    // TODO
-    d = sdSphere((p - vec3(0.0, 0.35, -0.7)), 0.35);
+    d = sdRoundCone(p - vec3(0.0, 0.35, -0.7), 0.4, 0.3, 0.3);
 
+    float left_arm = sdCapsule(p + vec3(0.45, -0.3, 0.6), vec3(0.3, 0.3, 0.0), vec3(0.0, 0.0, 0.0), 0.05);
+    if (left_arm < d) {
+        d = left_arm;
+    }
+    float right_arm = sdCapsule(p + vec3(-0.15, -0.57, 0.6), vec3(0.3, -0.3, 0), vec3(0.0, 0.0, 0.0), 0.05);
+    if (right_arm < d) {
+        d = right_arm;
+    }
+    float left_leg = sdVerticalCapsule(p + vec3(0.1, 0.03, 0.55), 0.05, 0.05);
+    if (left_leg < d) {
+        d = left_leg;
+    }
+    float right_leg = sdVerticalCapsule(p + vec3(-0.1, 0.03, 0.55), 0.05, 0.05);
+    if (right_leg < d) {
+        d = right_leg;
+    }
     // return distance and color
     return vec4(d, vec3(0.0, 1.0, 0.0));
 }
 
 vec4 sdEye(vec3 p)
 {
-
-    vec4 res = vec4(1e10, 0.0, 0.0, 0.0);
-
-    return res;
+    vec4 res_white = vec4(sdSphere(p - vec3(0, 0.6, -0.4), 0.17), 255, 255, 255);
+    vec4 res_blue = vec4(sdSphere(p - vec3(0, 0.59, -0.25), 0.1), 0, 255, 255);
+    vec4 res_black = vec4(sdSphere(p - vec3(0, 0.58, -0.2), 0.066), 0, 0, 0);
+    float min_x = min(res_white.x, min(res_blue.x, res_black.x));
+    if (res_blue.x == min_x) {
+        return res_blue;
+    }
+    if (res_black.x == min_x) {
+        return res_black;
+    }
+    return res_white;
 }
 
 vec4 sdMonster(vec3 p)
