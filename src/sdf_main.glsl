28be9f58c2a36a29f9389d2e76cf38af2f8799db
Ivan Kosolapov <54372667+Zymik@users.noreply.github.com>
2025-12-06 00:50:01 +0300

    Task09 Ivan Kosolapov ITMO

--- src/sdf_main.glsl
+++ src/sdf_main.glsl
@@ -1,8 +1,41 @@
 
+const vec3 center = vec3(0.0, 0.35, -0.7);
+const float delta_x_hand = 0.1;
+const float delta_y_hand = -0.1;
+const float radius_hand = 0.05;
+const float hand_x = 0.3;
+const float hand_y = 0.1;
+
 // sphere with center in (0, 0, 0)
 float sdSphere(vec3 p, float r)
 {
     return length(p) - r;
+}
+
+float sdRoundCone( vec3 p, float r1, float r2, float h )
+{
+  float b = (r1-r2)/h;
+  float a = sqrt(1.0-b*b);
+
+  vec2 q = vec2( length(p.xz), p.y );
+  float k = dot(q,vec2(-b,a));
+  if( k<0.0 ) return length(q) - r1;
+  if( k>a*h ) return length(q-vec2(0.0,h)) - r2;
+  return dot(q, vec2(a,b) ) - r1;
+}
+
+
+float sdCapsule( vec3 p, vec3 a, vec3 b, float r )
+{
+  vec3 pa = p - a, ba = b - a;
+  float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+  return length( pa - ba*h ) - r;
+}
+
+float sdVerticalCapsule( vec3 p, float h, float r )
+{
+  p.y -= clamp( p.y, 0.0, h );
+  return length( p ) - r;
 }
 
 // XZ plane
@@ -24,6 +57,7 @@ float lazycos(float angle)
     return 1.0;
 }
 
+
 // возможно, для конструирования тела пригодятся какие-то примитивы из набора https://iquilezles.org/articles/distfunctions/
 // способ сделать гладкий переход между примитивами: https://iquilezles.org/articles/smin/
 vec4 sdBody(vec3 p)
@@ -31,7 +65,7 @@ vec4 sdBody(vec3 p)
     float d = 1e10;
 
     // TODO
-    d = sdSphere((p - vec3(0.0, 0.35, -0.7)), 0.35);
+    d = sdRoundCone((p - center), 0.35, 0.27, 0.35);
 
     // return distance and color
     return vec4(d, vec3(0.0, 1.0, 0.0));
@@ -39,10 +73,51 @@ vec4 sdBody(vec3 p)
 
 vec4 sdEye(vec3 p)
 {
+    float d = 1e10;
 
-    vec4 res = vec4(1e10, 0.0, 0.0, 0.0);
+    // TODO
+    float y = 0.7;
+    float z = -0.5;
+    float x = 0.0;
+    d = sdSphere((p - vec3(x, y, z)), 0.17);
+    if (p.z > z) {
+        if (pow(abs(p.y - y), 2.0) + pow(abs(p.x - x), 2.0) < 0.003) {
+            return vec4(d, vec3(0.0, 0.0, 0.0));
+        }
+        if (pow(abs(p.y - y), 2.0) + pow(abs(p.x - x), 2.0) < 0.01) {
+            return vec4(d, vec3(0.188, 0.835, 0.784));
+        }
+    }
+    // return distance and color
+    return vec4(d, vec3(1.0, 1.0, 1.0));
+}
 
-    return res;
+vec4 sdLeftHand(vec3 p)
+{
+    float d = 1e10;
+    vec3 hand = vec3(-hand_x, hand_y, 0.0);
+    vec3 delta_hand = vec3(-delta_x_hand, lazycos(iTime * 7.0) * delta_y_hand, 0.0);
+    d = sdCapsule(p - center, hand, hand + delta_hand, radius_hand);
+    return vec4(d, vec3(0.0, 1.0, 0.0));
+}
+
+vec4 sdRightHand(vec3 p)
+{
+    float d = 1e10;
+    vec3 hand = vec3(hand_x, hand_y, 0.0);
+    vec3 delta_hand = vec3(delta_x_hand,  delta_y_hand, 0.0);
+    d = sdCapsule(p - center, hand, hand + delta_hand, radius_hand);
+    return vec4(d, vec3(0.0, 1.0, 0.0));
+}
+
+vec4 sdLeftLeg(vec3 p) {
+    float d = sdVerticalCapsule(p - center - vec3(-0.2, -0.38, 0.0), 0.15, radius_hand);
+    return vec4(d, vec3(0.0, 1.0, 0.0));
+}
+
+vec4 sdRightLeg(vec3 p) {
+    float d = sdVerticalCapsule(p - center - vec3(0.2, -0.38, 0.0), 0.15, radius_hand);
+    return vec4(d, vec3(0.0, 1.0, 0.0));
 }
 
 vec4 sdMonster(vec3 p)
@@ -52,10 +127,30 @@ vec4 sdMonster(vec3 p)
     p -= vec3(0.0, 0.08, 0.0);
 
     vec4 res = sdBody(p);
+   
 
     vec4 eye = sdEye(p);
     if (eye.x < res.x) {
         res = eye;
+    }
+    vec4 leftHand = sdLeftHand(p);
+    if (leftHand.x < res.x) {
+        res = leftHand;
+    }
+    
+    vec4 rightHand = sdRightHand(p);
+    if (rightHand.x < res.x) {
+        res = rightHand;
+    }
+    
+    vec4 leftLeg = sdLeftLeg(p);
+    if (leftLeg.x < res.x) {
+        res = leftLeg;
+    }
+    
+    vec4 rightLeg = sdRightLeg(p);
+    if (rightLeg.x < res.x) {
+        res = rightLeg;
     }
 
     return res;
