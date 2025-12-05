7a8e67796b8af8ac56f159501f1ecad97096ce6d
HackAss2002 <HackAss2002@inbox.ru>
2025-12-05 04:00:18 +0300

    Task09 Вячеслав Григорович ITMO

--- src/sdf_main.glsl
+++ src/sdf_main.glsl
@@ -1,56 +1,152 @@
+const int ITERS_COUNT = 256;
+const float EPS = 0.001;
+const float MAX_Y = 2.0;
 
-// sphere with center in (0, 0, 0)
+const float PI = 3.1415926535;
+
+float dot2( in vec2 v ) { return dot(v,v); }
+float dot2( in vec3 v ) { return dot(v,v); }
+
+float smin(float a, float b, float k)
+{
+    float h = clamp(0.5 + 0.5*(b-a)/k, 0.0, 1.0);
+    return mix(b, a, h) - k*h*(1.0-h);
+}
+
 float sdSphere(vec3 p, float r)
 {
     return length(p) - r;
 }
 
-// XZ plane
+float sdRoundBox(vec3 p, vec3 b, float r)
+{
+  vec3 q = abs(p) - b + r;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0) - r;
+}
+
 float sdPlane(vec3 p)
 {
     return p.y;
 }
 
-// косинус который пропускает некоторые периоды, удобно чтобы махать ручкой не все время
-float lazycos(float angle)
+float sdEllipsoid(vec3 p, vec3 r)
 {
-    int nsleep = 10;
-
-    int iperiod = int(angle / 6.28318530718) % nsleep;
-    if (iperiod < 3) {
-        return cos(angle);
-    }
-
-    return 1.0;
+  float k0 = length(p/r);
+  float k1 = length(p/(r*r));
+  return k0*(k0-1.0)/k1;
 }
 
-// возможно, для конструирования тела пригодятся какие-то примитивы из набора https://iquilezles.org/articles/distfunctions/
-// способ сделать гладкий переход между примитивами: https://iquilezles.org/articles/smin/
+float sdRoundCone(vec3 p, vec3 a, vec3 b, float r1, float r2)
+{
+  vec3  ba = b - a;
+  float l2 = dot(ba,ba);
+  float rr = r1 - r2;
+  float a2 = l2 - rr*rr;
+  float il2 = 1.0/l2;
+    
+  vec3 pa = p - a;
+  float y = dot(pa,ba);
+  float z = y - l2;
+  float x2 = dot2( pa*l2 - ba*y );
+  float y2 = y*y*l2;
+  float z2 = z*z*l2;
+
+  float k = sign(rr)*rr*rr*x2;
+  if( sign(z)*a2*z2>k ) return  sqrt(x2 + z2)        *il2 - r2;
+  if( sign(y)*a2*y2<k ) return  sqrt(x2 + y2)        *il2 - r1;
+                        return (sqrt(x2*a2*il2)+y*rr)*il2 - r1;
+}
+
+float sdRoundedCylinderHorizontal(vec3 p, float ra, float rb, float h) {
+    vec2 d = vec2(length(p.yz)-ra+rb, abs(p.x) - h + rb);
+    return min(max(d.x,d.y),0.0) + length(max(d,0.0)) - rb;
+}
+
+vec3 barbellPos() {
+    vec3 center = vec3(0.0, 1.0, -0.3);
+    float limit = 0.3;
+    float len = 0.3;
+    return center + vec3(0.0,
+        clamp(sin(iTime * 2.0), -sqrt(1.0 - limit * limit), sqrt(1.0 - limit * limit)),
+        max(abs(cos(iTime * 2.0)), limit)) * len;
+}
+
+vec4 sdBarbell(vec3 p) {
+    float d = 1e10;
+    float weightH = 0.025;
+    float weightR = 0.3;
+    float weightCount = 3.0;
+    float s = weightH * 2.0 + 0.005;
+    vec3 color = vec3(0.2, 0.2, 0.2);
+    
+    vec3 pq = p;
+    pq.x = abs(pq.x);
+    vec3 q = pq - vec3(0.75, 0.0, 0.0);
+    q.x = q.x - s*clamp(round(q.x/s), 0.0, 6.0);
+    d = sdRoundedCylinderHorizontal(q, weightR, 0.001, weightH);
+    
+    float barbell = sdRoundedCylinderHorizontal(p, 0.03, 0.001, 1.1);
+    barbell = min(barbell, sdRoundedCylinderHorizontal(pq - vec3(0.7, 0.0, 0.0), 0.05, 0.001, 0.05));
+    
+    if (barbell < d) {
+        d = barbell;
+        color = vec3(0.7, 0.7, 0.7);
+    }
+    
+    return vec4(d, color);
+}
+
 vec4 sdBody(vec3 p)
 {
     float d = 1e10;
-
-    // TODO
-    d = sdSphere((p - vec3(0.0, 0.35, -0.7)), 0.35);
-
-    // return distance and color
-    return vec4(d, vec3(0.0, 1.0, 0.0));
+    d = sdSphere((p - vec3(0.0, 1.4, -0.7)), 0.4);
+    d = smin(d, sdSphere((p - vec3(0.0, 1.7, -0.7)), 0.3), 0.25);
+    
+    vec3 q = p;
+    q.x = abs(q.x);
+    
+    // Chest
+    d = smin(d, sdRoundBox((p - vec3(0.17, 1.45, -0.35)), vec3(0.12), 0.07), 0.15);
+    d = smin(d, sdRoundBox((p - vec3(-0.17, 1.45, -0.35)), vec3(0.12), 0.07), 0.15);
+    
+    // Abs
+    d = smin(d, sdRoundBox(p - vec3(0.085, 1.3, -0.33), vec3(0.07), 0.03), 0.07);
+    d = smin(d, sdRoundBox(p - vec3(-0.085, 1.3, -0.33), vec3(0.07), 0.03), 0.07);
+    d = smin(d, sdRoundBox(p - vec3(0.085, 1.15, -0.35), vec3(0.07), 0.03), 0.07);
+    d = smin(d, sdRoundBox(p - vec3(-0.085, 1.15, -0.35), vec3(0.07), 0.03), 0.07);
+    
+    // Legs
+    d = smin(d, sdRoundCone(q, vec3(0.175, 1.1, -0.7), vec3(0.2, 0.6, -0.6), 0.15, 0.12), 0.07);
+    d = smin(d, sdRoundCone(q, vec3(0.2, 0.6, -0.6), vec3(0.18, 0.1, -0.7), 0.12, 0.1), 0.07);
+    d = smin(d, sdRoundBox(q - vec3(0.18, 0.03, -0.5), vec3(0.1, 0.05, 0.2), 0.03), 0.07);
+    
+    // Arms
+    d = smin(d, sdEllipsoid(q - vec3(0.4, 1.3, -0.5), vec3(0.12, 0.3, 0.1)), 0.07);
+    vec3 bPos = barbellPos() + vec3(0.4, 0.0, 0.0);
+    d = smin(d, sdRoundCone(q, vec3(0.43, 1.0, -0.5), bPos - normalize(bPos - vec3(0.43, 1.0, -0.5)) * 0.12, 0.1, 0.08), 0.07);
+    d = smin(d, sdSphere(q - bPos, 0.1), 0.03);
+    
+    return vec4(d, vec3(0.1, 1.0, 0.1));
 }
 
 vec4 sdEye(vec3 p)
 {
-
-    vec4 res = vec4(1e10, 0.0, 0.0, 0.0);
-
-    return res;
+    vec3 eyePos = vec3(0.0, 1.73, -0.4);
+    float d = sdSphere((p - eyePos), 0.15);
+    vec3 eyeDir = normalize(vec3(0.0, 0.3, 1.0));
+    vec3 color = vec3(1.0, 1.0, 1.0);
+    vec3 pointDir = normalize(p - eyePos);
+    if (dot(eyeDir, pointDir) >= 0.95) {
+        color = vec3(0.0, 0.0, 0.0);
+    }
+    else if (dot(eyeDir, pointDir) >= 0.8) {
+        color = vec3(0.0, 1.0, 1.0);
+    }
+    return vec4(d, color);
 }
 
-vec4 sdMonster(vec3 p)
+vec4 sdHorseCocker(vec3 p)
 {
-    // при рисовании сложного объекта из нескольких SDF, удобно на верхнем уровне
-    // модифицировать p, чтобы двигать объект как целое
-    p -= vec3(0.0, 0.08, 0.0);
-
     vec4 res = sdBody(p);
 
     vec4 eye = sdEye(p);
@@ -64,121 +160,184 @@ vec4 sdMonster(vec3 p)
 
 vec4 sdTotal(vec3 p)
 {
-    vec4 res = sdMonster(p);
-
+    vec4 res = vec4(1e10, 0.0, 0.0, 0.0);
+    float s = 4.0;
+    {
+        vec3 q = p - vec3(0.0, 0.0, -1.0);
+        q.x = q.x - s*round(q.x/s);
+        q.z = q.z - s*round(q.z/s);
+        res = sdHorseCocker(q);
+    }
+    
+    {
+        vec3 q = p - vec3(0.0, 0.0, -1.0) - barbellPos();
+        q.x = q.x - s*round(q.x/s);
+        q.z = q.z - s*round(q.z/s);
+        vec4 barbell = sdBarbell(q);
+        if (barbell.x < res.x) {
+            res = barbell;
+        }
+    }
 
     float dist = sdPlane(p);
     if (dist < res.x) {
-        res = vec4(dist, vec3(1.0, 0.0, 0.0));
+        float step = 1.0;
+        if (bool(int(p.x - step * floor(p.x / step) > step / 2.0) ^
+            int(p.z - step * floor(p.z / step) > step / 2.0))) {
+            res = vec4(dist, vec3(1.0));
+        } else {
+            res = vec4(dist, vec3(0.0));
+        }
     }
 
     return res;
 }
 
-// see https://iquilezles.org/articles/normalsSDF/
-vec3 calcNormal( in vec3 p ) // for function f(p)
+vec3 calcNormal(in vec3 p)
 {
-    const float eps = 0.0001; // or some other value
+    const float h = 0.0001;
+    #define ZERO (min(iFrame,0))
+    vec3 n = vec3(0.0);
+    for( int i=ZERO; i<4; i++ )
+    {
+        vec3 e = 0.5773*(2.0*vec3((((i+3)>>1)&1),((i>>1)&1),(i&1))-1.0);
+        n += e*sdTotal(p+e*h).x;
+    }
+    return normalize(n);
+    /*const float eps = 0.0001;
     const vec2 h = vec2(eps,0);
     return normalize( vec3(sdTotal(p+h.xyy).x - sdTotal(p-h.xyy).x,
     sdTotal(p+h.yxy).x - sdTotal(p-h.yxy).x,
-    sdTotal(p+h.yyx).x - sdTotal(p-h.yyx).x ) );
+    sdTotal(p+h.yyx).x - sdTotal(p-h.yyx).x ) );*/
 }
 
 
-vec4 raycast(vec3 ray_origin, vec3 ray_direction)
+struct RayCastResult
 {
+    float t;
+    vec3 color;
+    bool hasHit;
+};
 
-    float EPS = 1e-3;
-
-
-    // p = ray_origin + t * ray_direction;
-
+RayCastResult raycast(vec3 rayOrig, vec3 rayDir)
+{
     float t = 0.0;
-
-    for (int iter = 0; iter < 200; ++iter) {
-        vec4 res = sdTotal(ray_origin + t*ray_direction);
+    for (int iter = 0; iter < ITERS_COUNT; ++iter) {
+        vec4 res = sdTotal(rayOrig + t * rayDir);
         t += res.x;
         if (res.x < EPS) {
-            return vec4(t, res.yzw);
+            return RayCastResult(t, res.yzw, true);
+        }
+        
+        if (rayOrig.y + rayDir.y * t > MAX_Y){
+            break;
         }
     }
-
-    return vec4(1e10, vec3(0.0, 0.0, 0.0));
+    return RayCastResult(t, vec3(0.0, 0.0, 0.0), false);
 }
 
-
-float shading(vec3 p, vec3 light_source, vec3 normal)
+vec3 shade(vec3 normal, vec3 ka, vec3 kd, vec3 ks, float shininess, vec3 lightDir, vec3 lightColor, float shadow, vec3 cameraDir)
 {
-
-    vec3 light_dir = normalize(light_source - p);
-
-    float shading = dot(light_dir, normal);
-
-    return clamp(shading, 0.5, 1.0);
-
-}
-
-// phong model, see https://en.wikibooks.org/wiki/GLSL_Programming/GLUT/Specular_Highlights
-float specular(vec3 p, vec3 light_source, vec3 N, vec3 camera_center, float shinyness)
-{
-    vec3 L = normalize(p - light_source);
-    vec3 R = reflect(L, N);
-
-    vec3 V = normalize(camera_center - p);
-
-    return pow(max(dot(R, V), 0.0), shinyness);
-}
-
-
-float castShadow(vec3 p, vec3 light_source)
-{
-
-    vec3 light_dir = p - light_source;
-
-    float target_dist = length(light_dir);
-
-
-    if (raycast(light_source, normalize(light_dir)).x + 0.001 < target_dist) {
-        return 0.5;
+    vec3 L = -lightDir;
+    vec3 V = -cameraDir;
+    vec3 H = normalize(L + V);
+    
+    vec3 ambient = ka * lightColor;
+    
+    float diff = max(dot(normal, L), 0.0) * shadow;
+    vec3 diffuse = kd * diff * lightColor;
+    
+    float spec = 0.0;
+    if (diff > 0.0) {
+        spec = pow(max(dot(normal, H), 0.0), shininess);
     }
-
-    return 1.0;
+    vec3 specular = ks * spec * lightColor;
+    
+    return ambient + diffuse + specular;
 }
 
+float castShadowDirLight(vec3 p, vec3 lightDir)
+{
+    float w = 0.05;
+    float res = 1.0;
+    float ph = 1e20;
+    float t = 0.005;
+    for(int i = 0; i < ITERS_COUNT; ++i)
+    {
+        float h = sdTotal(p - lightDir * t).x;
+        if(h < 0.001) {
+            return 0.0;
+        }
+        float y = h * h / (2.0 * ph);
+        float d = sqrt(h * h - y * y);
+        res = min(res, d / (w * max(0.0, t - y)));
+        ph = h;
+        t += h;
+        if (p.y - lightDir.y * t > MAX_Y){
+            break;
+        }
+    }
+    return res;
+}
+
+struct DirLight {
+    vec3 dir;
+    vec3 color;
+};
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 uv = fragCoord/iResolution.y;
-
     vec2 wh = vec2(iResolution.x / iResolution.y, 1.0);
+    
+    vec3 cameraPos = vec3(0.0, 1.8, 1.0);
+    vec3 cameraDir = vec3(0.0, 0.0, -1.0);
+    
+    vec3 rayOrig = cameraPos;
+    vec3 rayDir = normalize(vec3(uv - 0.5*wh, -1.0));
 
+    RayCastResult res = raycast(rayOrig, rayDir);
+    
+    const int dirLightsCount = 4;
+    float colorIntens = 0.8;
+    DirLight dirLights[dirLightsCount] = DirLight[dirLightsCount](
+        DirLight(normalize(vec3(1.0, -1.0, -1.0)), vec3(1.0) * 0.3),
+        DirLight(normalize(vec3(sin(iTime * PI), -0.5, cos(iTime * PI))), vec3(1.0, 0.0, 0.0) * colorIntens),
+        DirLight(normalize(vec3(sin(iTime * PI + PI * 2.0 / 3.0), -0.5, cos(iTime * PI + PI * 2.0 / 3.0))), vec3(0.0, 1.0, 0.0) * colorIntens),
+        DirLight(normalize(vec3(sin(iTime * PI + PI * 4.0 / 3.0), -0.5, cos(iTime * PI + PI * 4.0 / 3.0))), vec3(0.0, 0.0, 1.0) * colorIntens)
+    );
+    
+    if (!res.hasHit) {
+        fragColor = mix(vec4(0.3, 0.3, 0.9, 1.0), vec4(0.1, 0.1, 0.3, 1.0), (uv.y - 0.5) * 2.0);
+        return;
+    }
+    
+    /*vec3 ka = res.color * 0.1;
+    vec3 kd = res.color * 0.9;
+    vec3 ks = vec3(0.1);
+    float shininess = 30.0;*/
+    
+    vec3 ka = res.color * 0.1;
+    vec3 kd = res.color * 0.8;
+    vec3 ks = vec3(0.2);
+    float shininess = 30.0;
+    
+    
+    /*vec3 ka = res.color * 0.1;
+    vec3 kd = res.color * 0.6;
+    vec3 ks = vec3(0.3);
+    float shininess = 30.0;*/
 
-    vec3 ray_origin = vec3(0.0, 0.5, 1.0);
-    vec3 ray_direction = normalize(vec3(uv - 0.5*wh, -1.0));
+    vec3 pos = rayOrig + res.t * rayDir;
+    vec3 normal = calcNormal(pos);
+    
+    vec3 col = ka * 0.05;
+    
+    for (int i = 0; i < dirLightsCount; ++i) {
+        DirLight light = dirLights[i];
+        float shadow = castShadowDirLight(pos, light.dir);
+        col += shade(normal, ka, kd, ks, shininess, light.dir, light.color, shadow, cameraDir);
+    }
 
-
-    vec4 res = raycast(ray_origin, ray_direction);
-
-
-
-    vec3 col = res.yzw;
-
-
-    vec3 surface_point = ray_origin + res.x*ray_direction;
-    vec3 normal = calcNormal(surface_point);
-
-    vec3 light_source = vec3(1.0 + 2.5*sin(iTime), 10.0, 10.0);
-
-    float shad = shading(surface_point, light_source, normal);
-    shad = min(shad, castShadow(surface_point, light_source));
-    col *= shad;
-
-    float spec = specular(surface_point, light_source, normal, ray_origin, 30.0);
-    col += vec3(1.0, 1.0, 1.0) * spec;
-
-
-
-    // Output to screen
     fragColor = vec4(col, 1.0);
 }
