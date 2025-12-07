9e26d456f914d1124a77feaeb5c70a99c93194fb
Adelina Alimova <mayakovskiygay@mail.ru>
2025-12-07 13:24:05 +0300

    Task09 Аделина Алимова HSE

--- src/sdf_main.glsl
+++ src/sdf_main.glsl
@@ -12,16 +12,69 @@ float sdPlane(vec3 p)
 }
 
 // косинус который пропускает некоторые периоды, удобно чтобы махать ручкой не все время
-float lazycos(float angle)
+float lazycos(float angle, int nsleep, int period)
 {
-    int nsleep = 10;
+    //int nsleep = 10;
 
     int iperiod = int(angle / 6.28318530718) % nsleep;
-    if (iperiod < 3) {
+    if (iperiod < period) {
         return cos(angle);
     }
 
     return 1.0;
+}
+
+float smin(float a, float b, float k)
+{
+    float h = clamp(0.5 + 0.5 * (b - a) / k, 0.0, 1.0);
+    return mix(b, a, h) - k * h * (1.0 - h);
+}
+
+float sdCapsule(vec3 p, vec3 a, vec3 b, float r)
+{
+    vec3 pa = p - a;
+    vec3 ba = b - a;
+    float h = clamp(dot(pa, ba) / dot(ba, ba), 0.0, 1.0);
+    return length(pa - ba * h) - r;
+}
+
+float sdArms(vec3 p) {
+    vec3 c = vec3(0.0, 0.35, -0.7);
+
+    float swing = 0.1 * (lazycos(iTime * 8.0, 10, 3) - 1.0);
+
+    vec3 armL_a = c + vec3(0.3, 0.05, 0.0);
+    vec3 armL_b = c + vec3(0.37, -0.05, 0.05);
+
+
+    vec3 armR_a = c + vec3(-0.3, 0.05, 0.0);
+    vec3 armR_b = c + vec3(-0.37, -0.05, 0.05);
+
+    vec3 dirR = armR_b - armR_a;
+    float lenR = length(dirR);
+    dirR.y -= swing;
+    dirR = normalize(dirR);
+    armR_b = armR_a + dirR * lenR;
+
+    float armL = sdCapsule(p, armL_a, armL_b, 0.05);
+    float armR = sdCapsule(p, armR_a, armR_b, 0.05);
+
+    return min(armL, armR);
+}
+
+float sdLegs(vec3 p) {
+    vec3 c = vec3(0.0, 0.35, -0.7);
+
+    vec3 legL_a = c + vec3(0.12, -0.10, 0.0);
+    vec3 legL_b = c + vec3(0.12, -0.4, 0.05);
+
+    vec3 legR_a = c + vec3(-0.12, -0.10, 0.0);
+    vec3 legR_b = c + vec3(-0.12, -0.4, 0.05);
+
+    float legL = sdCapsule(p, legL_a, legL_b, 0.065);
+    float legR = sdCapsule(p, legR_a, legR_b, 0.065);
+
+    return min(legL, legR);
 }
 
 // возможно, для конструирования тела пригодятся какие-то примитивы из набора https://iquilezles.org/articles/distfunctions/
@@ -30,19 +83,57 @@ vec4 sdBody(vec3 p)
 {
     float d = 1e10;
 
-    // TODO
-    d = sdSphere((p - vec3(0.0, 0.35, -0.7)), 0.35);
+    float body = sdSphere((p - vec3(0.0, 0.35, -0.7)), 0.35);
+    float head = sdSphere((p - vec3(0.0, 0.65, -0.7)), 0.2);
+
+    float arms = sdArms(p);
+    float legs = sdLegs(p);
+
+    d = smin(body, head, 0.25);
+    d = smin(d, arms, 0.02);
+    d = smin(d, legs, 0.04);
 
     // return distance and color
-    return vec4(d, vec3(0.0, 1.0, 0.0));
+    return vec4(d, vec3(0.0, 0.8, 0.0));
 }
 
 vec4 sdEye(vec3 p)
 {
 
-    vec4 res = vec4(1e10, 0.0, 0.0, 0.0);
+    vec3 c = vec3(0.0, 0.35, -0.7);
+    vec3 e = c + vec3(0.0, 0.22, 0.2);
 
-    return res;
+    float sclera = sdSphere(p - e, 0.18);
+    float iris = sdSphere(p - (e + vec3(0.0, 0.004, 0.037)), 0.15);
+    float pupil = sdSphere(p - (e + vec3(0.0, 0.01, 0.15)), 0.065);
+
+    float d = sclera;
+    vec3 col = vec3(1.0);
+
+    if (iris < d) {
+        d = iris;
+        col = vec3(0.3, 0.9, 0.9);
+    }
+
+    if (pupil < d) {
+        d = pupil;
+        col = vec3(0.0);
+    }
+
+    float blink = lazycos(4.0 * iTime, 7, 1) - 1.0;
+
+    vec3 ep = p - e;
+    float lid_bottom = ep.y - (-0.2 - 0.11 * blink);
+    float lid_top = -ep.y - (-0.2 - 0.11 * blink);
+    float lid = min(lid_bottom, lid_top);
+    float eyeLimit = sdSphere(ep - vec3(0.0, 0.0, 0.015), 0.18);
+    lid = max(lid, eyeLimit);
+    if (lid < sclera) {
+        d = lid;
+        col = vec3(0.0, 1.0, 0.0);
+    }
+
+    return vec4(d, col);
 }
 
 vec4 sdMonster(vec3 p)
