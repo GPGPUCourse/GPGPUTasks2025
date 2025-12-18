cd5e0c60c33640b3cf5b657719de1c27012a4313
Arthur Berseneow <orturB@yandex.ru>
2025-12-19 00:26:37 +0300

    Done: task06

--- src/kernels/cu/merge_sort.cu
+++ src/kernels/cu/merge_sort.cu
@@ -7,24 +7,85 @@
 #include "helpers/rassert.cu"
 #include "../defines.h"
 
-__global__ void merge_sort(
-    const unsigned int* input_data,
-          unsigned int* output_data,
-                   int  sorted_k,
-                   int  n)
-{
-    const int i = blockIdx.x * blockDim.x + threadIdx.x;
-    // TODO
+using u32 = unsigned int;
+
+__global__ void GPUMerge(u32* in_buf, u32* out_buf, u32 n, u32 new_chunk_size) {
+    u32 i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (i >= n) {
+        return;
+    }
+
+    u32 pos_in_new_chunk = i % new_chunk_size;
+    u32 curr_chunk_size = new_chunk_size >> 1;
+
+    bool is_left_half = (pos_in_new_chunk < curr_chunk_size);
+
+    u32 curr_chunk_idx = i / curr_chunk_size;
+
+    // [adjacent_chunk_l; adjacent_chunk_r)
+    const u32 adjacent_chunk_l = (is_left_half * (curr_chunk_idx+1) * curr_chunk_size)
+                               + (!is_left_half * (curr_chunk_idx-1) * curr_chunk_size);
+
+    const u32 adjacent_chunk_r = min(adjacent_chunk_l + curr_chunk_size, n);
+    u32 curr_value = in_buf[i];
+
+    if (is_left_half && adjacent_chunk_l >= n) {
+        out_buf[i] = curr_value;
+        return;
+    }
+    // curassert(adjacent_chunk_l < adjacent_chunk_r, 321312312);
+    // curassert(adjacent_chunk_l < n, 897210);
+    // curassert(adjacent_chunk_r < n, 821893211);
+
+    u32 new_chunk_idx = i / new_chunk_size;
+    // printf("i=%d l=%d, r=%d, curr_chunk_idx=%d, new_chunk_idx=%d, is_left=%d\n", i,
+    //         adjacent_chunk_l, adjacent_chunk_r, curr_chunk_idx, new_chunk_idx, is_left_half);
+
+    // COMPUTE OFFSET IN ADJACENT CHUNK OF CURRENT SIZE
+    u32 l = adjacent_chunk_l;
+    u32 r = adjacent_chunk_r;
+
+    u32 offset_in_current_chunk =  i % curr_chunk_size;
+    u32 offset_in_ajacent_chunk = 0; // UPDATE VIA BINARY SEARCH
+
+
+    while (l < r) {
+        u32 mid = l + ((r - l) >> 1);
+        u32 mid_value = in_buf[mid];
+
+        bool go_right = is_left_half ? (mid_value < curr_value) : (mid_value <= curr_value);
+        l = go_right * (mid+1) + !go_right * (l);
+        r = go_right * (r) + !go_right * (mid);
+    }
+
+    offset_in_ajacent_chunk = r - adjacent_chunk_l;
+
+    u32 output_res = (new_chunk_size * new_chunk_idx) + offset_in_current_chunk + offset_in_ajacent_chunk;
+    // printf("%d > i(%d) curr_chunk_offset=%d aj_offset=%d output_res=%d\n", curr_value, i, offset_in_current_chunk, offset_in_ajacent_chunk, output_res);
+    out_buf[output_res] = curr_value;
 }
 
 namespace cuda {
-void merge_sort(const gpu::WorkSize &workSize,
-            const gpu::gpu_mem_32u &input_data, gpu::gpu_mem_32u &output_data, int sorted_k, int n)
-{
+
+void MergeSort(gpu::gpu_mem_32u& b1, gpu::gpu_mem_32u& b2, int n) {
     gpu::Context context;
     rassert(context.type() == gpu::Context::TypeCUDA, 34523543124312, context.type());
     cudaStream_t stream = context.cudaStream();
-    ::merge_sort<<<workSize.cuGridSize(), workSize.cuBlockSize(), 0, stream>>>(input_data.cuptr(), output_data.cuptr(), sorted_k, n);
-    CUDA_CHECK_KERNEL(stream);
+    gpu::WorkSize ws(GROUP_SIZE, n);
+    bool invert = false;
+    for (std::size_t new_chunk_size = 2; (new_chunk_size / 2) < n; new_chunk_size <<= 1) {
+        ::GPUMerge<<<ws.cuGridSize(), ws.cuBlockSize(), 0, stream>>>
+                  (b1.cuptr(), b2.cuptr(), n, new_chunk_size);
+        CUDA_CHECK_KERNEL(stream);
+        std::swap(b1, b2);
+        invert = !invert;
+    }
+
+    if (invert) {
+        std::swap(b1, b2);
+    }
+
 }
+
 } // namespace cuda
--- src/main_merge_sort.cpp
+++ src/main_merge_sort.cpp
@@ -26,7 +26,7 @@ void run(int argc, char** argv)
     // TODO 000 P.S. если вы выбрали CUDA - не забудьте установить CUDA SDK и добавить -DCUDA_SUPPORT=ON в CMake options
     // TODO 010 P.S. так же в случае CUDA - добавьте в CMake options (НЕ меняйте сами CMakeLists.txt чтобы не менять окружение тестирования):
     // TODO 010 "-DCMAKE_CUDA_ARCHITECTURES=75 -DCMAKE_CUDA_FLAGS=-lineinfo" (первое - чтобы включить поддержку WMMA, второе - чтобы compute-sanitizer и профилировщик знали номера строк кернела)
-    gpu::Context context = activateContext(device, gpu::Context::TypeOpenCL);
+    gpu::Context context = activateContext(device, gpu::Context::TypeCUDA);
     // OpenCL - рекомендуется как вариант по умолчанию, можно выполнять на CPU, есть printf, есть аналог valgrind/cuda-memcheck - https://github.com/jrprice/Oclgrind
     // CUDA   - рекомендуется если у вас NVIDIA видеокарта, есть printf, т.к. в таком случае вы сможете пользоваться профилировщиком (nsight-compute) и санитайзером (compute-sanitizer, это бывший cuda-memcheck)
     // Vulkan - не рекомендуется, т.к. писать код (compute shaders) на шейдерном языке GLSL на мой взгляд менее приятно чем в случае OpenCL/CUDA
@@ -46,9 +46,10 @@ void run(int argc, char** argv)
     std::vector<unsigned int> as(n, 0);
     std::vector<unsigned int> sorted(n, 0);
     for (size_t i = 0; i < n; ++i) {
-        as[i] = r.next(min_value, max_value);
+         as[i] = r.next(min_value, max_value);
     }
-    std::cout << "n=" << n << " values in range [" << min_value << "; " << max_value << "]" << std::endl;
+    std::cout << "n=" << n << " values in range [" << min_value
+              << "; " << max_value << "]" << std::endl;
 
     {
         // убедимся что в массиве есть хотя бы несколько повторяющихся значений
@@ -73,12 +74,15 @@ void run(int argc, char** argv)
         // Вычисляем достигнутую эффективную пропускную способность видеопамяти (из соображений что мы отработали в один проход - считали массив и сохранили его переупорядоченным)
         double memory_size_gb = sizeof(unsigned int) * 2 * n / 1024.0 / 1024.0 / 1024.0;
         std::cout << "CPU std::sort finished in " << t.elapsed() << " sec" << std::endl;
-        std::cout << "CPU std::sort effective RAM bandwidth: " << memory_size_gb / t.elapsed() << " GB/s (" << n / 1000 / 1000 / t.elapsed() << " uint millions/s)" << std::endl;
+        std::cout << "CPU std::sort effective RAM bandwidth: "
+                  << memory_size_gb / t.elapsed() <<
+                  " GB/s (" << n / 1000 / 1000 / t.elapsed() <<
+                  " uint millions/s)" << std::endl;
     }
 
     // Аллоцируем буферы в VRAM
     gpu::gpu_mem_32u input_gpu(n);
-    gpu::gpu_mem_32u buffer1_gpu(n), buffer2_gpu(n); // TODO это просто шаблонка, можете переименовать эти буферы, сделать другого размера/типа, удалить часть, добавить новые
+    gpu::gpu_mem_32u buffer1_gpu(n), buffer2_gpu(n);
     gpu::gpu_mem_32u buffer_output_gpu(n);
 
     // Прогружаем входные данные по PCI-E шине: CPU RAM -> GPU VRAM
@@ -92,31 +96,26 @@ void run(int argc, char** argv)
 
     // Запускаем кернел (несколько раз и с замером времени выполнения)
     std::vector<double> times;
-    for (int iter = 0; iter < 10; ++iter) { // TODO при отладке запускайте одну итерацию
-        timer t;
-
-        // Запускаем кернел, с указанием размера рабочего пространства и передачей всех аргументов
-        // Если хотите - можете удалить ветвление здесь и оставить только тот код который соответствует вашему выбору API
-        if (context.type() == gpu::Context::TypeOpenCL) {
-            // TODO
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-        } else if (context.type() == gpu::Context::TypeCUDA) {
-            // TODO
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-        } else if (context.type() == gpu::Context::TypeVulkan) {
-            // TODO
-            throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-        } else {
-            rassert(false, 4531412341, context.type());
+    for (int iter = 0; iter < 10; ++iter) {
+        buffer1_gpu.writeN(as.data(), n);
+        {
+            timer t;
+            cuda::MergeSort(buffer1_gpu, buffer_output_gpu, n);
+            times.push_back(t.elapsed());
         }
 
-        times.push_back(t.elapsed());
     }
-    std::cout << "GPU merge-sort times (in seconds) - " << stats::valuesStatsLine(times) << std::endl;
 
+    // std::cout << "====>>>> GPU sorting res\n" << stats::vectorToString(buffer_output_gpu.readVector()) << std::endl;
+
+    std::cout << "GPU merge-sort times (in seconds) - "
+              << stats::valuesStatsLine(times) << std::endl;
     // Вычисляем достигнутую эффективную пропускную способность видеопамяти (из соображений что мы отработали в один проход - считали массив и сохранили его переупорядоченным)
     double memory_size_gb = sizeof(unsigned int) * 2 * n / 1024.0 / 1024.0 / 1024.0;
-    std::cout << "GPU merge-sort median effective VRAM bandwidth: " << memory_size_gb / stats::median(times) << " GB/s (" << n / 1000 / 1000 / stats::median(times) << " uint millions/s)" << std::endl;
+    std::cout << "GPU merge-sort median effective VRAM bandwidth: "
+              << memory_size_gb / stats::median(times) << " GB/s ("
+              << n / 1000 / 1000 / stats::median(times) << " uint millions/s)"
+              << std::endl;
 
     // Считываем результат по PCI-E шине: GPU VRAM -> CPU RAM
     std::vector<unsigned int> gpu_sorted = buffer_output_gpu.readVector();
