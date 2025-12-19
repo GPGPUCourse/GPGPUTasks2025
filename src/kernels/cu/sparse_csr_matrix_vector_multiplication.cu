6a716055e5e880b502c62be9e1d8a62217192306
Arthur Berseneow <orturB@yandex.ru>
2025-12-19 17:56:42 +0300

    Done: task07

--- src/kernels/cu/sparse_csr_matrix_vector_multiplication.cu
+++ src/kernels/cu/sparse_csr_matrix_vector_multiplication.cu
@@ -7,21 +7,71 @@
 #include "helpers/rassert.cu"
 #include "../defines.h"
 
-__global__ void sparse_csr_matrix_vector_multiplication() // TODO input/output buffers
+
+using u32 = unsigned int;
+
+__global__ void sparse_csr_matrix_vector_multiplication(
+    const  u32* csr_row_offsets,
+    const  u32* csr_columns,
+    const  u32* csr_values,
+    const  u32* vector_values,
+    u32* output_buffer,
+    const u32 nrows,
+    const u32 nnz)
 {
-    // TODO
+    const u32 row = blockIdx.x;
+
+    if (row >= nrows) return;
+
+    const u32 tid = threadIdx.x;
+    const u32 row_begin = csr_row_offsets[row];
+    const u32 row_end = (row + 1 < nrows) ? csr_row_offsets[row + 1] : nnz;
+
+    uint32_t partial_sum = 0;
+
+    for (uint32_t i = row_begin + tid; i < row_end; i += blockDim.x) {
+        partial_sum += csr_values[i] * vector_values[csr_columns[i]];
+    }
+
+    atomicAdd(&output_buffer[row], partial_sum);
+}
+
+__global__ void fill_zeroes(u32* output_buffer, u32 n) {
+    const u32 i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i >= n) {
+        return;
+    }
+    output_buffer[i] = 0;
 }
 
 namespace cuda {
-void sparse_csr_matrix_vector_multiplication(const gpu::WorkSize &workSize) // TODO input/output buffers
+
+void sparse_csr_matrix_vector_multiplication(
+    const gpu::WorkSize &workSize,
+    const  u32* csr_row_offsets,
+    const  u32* csr_columns,
+    const  u32* csr_values,
+    const  u32* vector_values,
+    u32* output_buffer,
+    const u32 nrows,
+    const u32 nnz
+)
 {
     gpu::Context context;
     rassert(context.type() == gpu::Context::TypeCUDA, 34523543124312, context.type());
     cudaStream_t stream = context.cudaStream();
+    gpu::WorkSize ws{GROUP_SIZE, nrows};
+    ::fill_zeroes<<<ws.cuGridSize(), ws.cuBlockSize(), 0, stream>>>(output_buffer, nrows);
     ::sparse_csr_matrix_vector_multiplication<<<workSize.cuGridSize(), workSize.cuBlockSize(), 0, stream>>>(
-        // TODO input/output buffers
-        // input_buffer.cuptr(),
-        );
+        csr_row_offsets,
+        csr_columns,
+        csr_values,
+        vector_values,
+        output_buffer,
+        nrows,
+        nnz
+    );
     CUDA_CHECK_KERNEL(stream);
 }
+
 } // namespace cuda
--- src/kernels/kernels.h
+++ src/kernels/kernels.h
@@ -1,11 +1,21 @@
 #pragma once
 
 #include <libgpu/vulkan/engine.h>
+#include "defines.h"
 
+
+using u32 = unsigned int;
 namespace cuda {
 void aplusb(const gpu::WorkSize& workSize, const gpu::gpu_mem_32u& a, const gpu::gpu_mem_32u& b, gpu::gpu_mem_32u& c, unsigned int n);
-void sparse_csr_matrix_vector_multiplication(const gpu::WorkSize &workSize); // TODO input/output buffers
-}
+
+void sparse_csr_matrix_vector_multiplication(const gpu::WorkSize &workSize,
+                                             const u32* csr_row_offsets,
+                                             const u32* csr_columns,
+                                             const u32* csr_values,
+                                             const  u32* vector_values,
+                                             u32* output_buffer,
+                                             const u32 nrows, const u32 nnz);
+} // namespace cuda
 
 namespace ocl {
 const ProgramBinaries& getAplusB();
