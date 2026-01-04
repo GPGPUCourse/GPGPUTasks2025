9d57b0db54b30c86721b388772544859f8409316
daniil1112 <moiplansh028@gmail.com>
2026-01-04 20:15:20 +0300

    Init solution

--- src/kernels/kernels.cpp
+++ src/kernels/kernels.cpp
@@ -3,12 +3,10 @@
 #include "cl/generated_kernels/aplusb.h"
 #include "cl/generated_kernels/fill_buffer_with_zeros.h"
 #include "cl/generated_kernels/prefix_sum_01_reduction.h"
-#include "cl/generated_kernels/prefix_sum_02_prefix_accumulation.h"
 
 #include "vk/generated_kernels/aplusb_comp.h"
 #include "vk/generated_kernels/fill_buffer_with_zeros_comp.h"
 #include "vk/generated_kernels/prefix_sum_01_reduction_comp.h"
-#include "vk/generated_kernels/prefix_sum_02_prefix_accumulation_comp.h"
 
 #ifndef CUDA_SUPPORT
 namespace cuda {
@@ -24,17 +22,11 @@ void fill_buffer_with_zeros(const gpu::WorkSize &workSize,
     // dummy implementation if CUDA_SUPPORT is disabled
     rassert(false, 54623523412413);
 }
-void prefix_sum_01_sum_reduction(const gpu::WorkSize &workSize,
+void prefix_sum_01_reduction(const gpu::WorkSize &workSize,
             const gpu::gpu_mem_32u &pow2_sum, gpu::gpu_mem_32u &next_pow2_sum, unsigned int n)
 {
     // dummy implementation if CUDA_SUPPORT is disabled
     rassert(false, 54623523412413);
-}
-void prefix_sum_02_prefix_accumulation(const gpu::WorkSize &workSize,
-            const gpu::gpu_mem_32u &pow2_sum, gpu::gpu_mem_32u &prefix_sum_accum, unsigned int n, unsigned int pow2)
-{
-    // dummy implementation if CUDA_SUPPORT is disabled
-    rassert(false, 546237686412414);
 }
 } // namespace cuda
 #endif
@@ -55,11 +47,7 @@ const ProgramBinaries& getPrefixSum01Reduction()
     return opencl_binaries_prefix_sum_01_reduction;
 }
 
-const ProgramBinaries& getPrefixSum02PrefixAccumulation()
-{
-    return opencl_binaries_prefix_sum_02_prefix_accumulation;
 }
-} // namespace ocl
 
 namespace avk2 {
 const ProgramBinaries& getAplusB()
@@ -77,8 +65,4 @@ const ProgramBinaries& getPrefixSum01Reduction()
     return vulkan_binaries_prefix_sum_01_reduction_comp;
 }
 
-const ProgramBinaries& getPrefixSum02PrefixAccumulation()
-{
-    return vulkan_binaries_prefix_sum_02_prefix_accumulation_comp;
-}
 } // namespace avk2
