03c27dfeb3214ddcba98a0fb3c26b13a05dda671
Anton <pan300503@gmail.com>
2026-01-05 23:37:38 +1100

    solution

--- src/kernels/cu/sum_03_local_memory_atomic_per_workgroup.cu
+++ src/kernels/cu/sum_03_local_memory_atomic_per_workgroup.cu
@@ -1,6 +1,6 @@
 #include <libgpu/context.h>
-#include <libgpu/work_size.h>
 #include <libgpu/shared_device_buffer.h>
+#include <libgpu/work_size.h>
 
 #include <libgpu/cuda/cu/common.cu>
 
@@ -9,20 +9,37 @@
 __global__ void sum_03_local_memory_atomic_per_workgroup(
     const unsigned int* a,
     unsigned int* sum,
-    unsigned int  n)
+    unsigned int n)
 {
-    // Подсказки:
-    // const uint index = blockIdx.x * blockDim.x + threadIdx.x;
-    // const uint local_index = threadIdx.x;
-    // __shared__ unsigned int local_data[GROUP_SIZE];
-    // __syncthreads();
+    __shared__ unsigned int local_data[GROUP_SIZE];
 
-    // TODO
+    const unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int local_index = threadIdx.x;
+    const unsigned int BLOCK_SIZE = n / LOAD_K_VALUES_PER_ITEM;
+
+    unsigned int my_sum = 0;
+    if (index < BLOCK_SIZE) {
+        for (unsigned int i = 0; i < LOAD_K_VALUES_PER_ITEM; i++) {
+            my_sum += a[i * BLOCK_SIZE + index];
+        }
+    }
+
+    local_data[local_index] = my_sum;
+    __syncthreads();
+
+    if (local_index != 0) {
+        return;
+    }
+    unsigned int block_sum = 0;
+    for (unsigned int i = 0; i < GROUP_SIZE; i++) {
+        block_sum += local_data[i];
+    }
+    atomicAdd(sum, block_sum);
 }
 
 namespace cuda {
-void sum_03_local_memory_atomic_per_workgroup(const gpu::WorkSize &workSize,
-    const gpu::gpu_mem_32u &a, gpu::gpu_mem_32u &sum, unsigned int n)
+void sum_03_local_memory_atomic_per_workgroup(const gpu::WorkSize& workSize,
+    const gpu::gpu_mem_32u& a, gpu::gpu_mem_32u& sum, unsigned int n)
 {
     gpu::Context context;
     rassert(context.type() == gpu::Context::TypeCUDA, 6573652345243, context.type());
--- src/kernels/cu/sum_04_local_reduction.cu
+++ src/kernels/cu/sum_04_local_reduction.cu
@@ -1,6 +1,6 @@
 #include <libgpu/context.h>
-#include <libgpu/work_size.h>
 #include <libgpu/shared_device_buffer.h>
+#include <libgpu/work_size.h>
 
 #include <libgpu/cuda/cu/common.cu>
 
@@ -11,20 +11,39 @@
 __global__ void sum_04_local_reduction(
     const unsigned int* a,
     unsigned int* b,
-    unsigned int  n)
+    unsigned int n)
 {
-    // Подсказки:
-    // const uint index = blockIdx.x * blockDim.x + threadIdx.x;
-    // const uint local_index = threadIdx.x;
-    // __shared__ unsigned int local_data[GROUP_SIZE];
-    // __syncthreads();
+    __shared__ unsigned int local_data[GROUP_SIZE];
 
-    // TODO
+    const unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int local_index = threadIdx.x;
+    const unsigned int block_index = blockIdx.x;
+    const unsigned int BLOCK_SIZE = (n + LOAD_K_VALUES_PER_ITEM - 1) / LOAD_K_VALUES_PER_ITEM;
+
+    unsigned int thread_sum = 0;
+    if (index < BLOCK_SIZE) {
+        for (unsigned int i = 0; i < LOAD_K_VALUES_PER_ITEM; i++) {
+            thread_sum += a[i * BLOCK_SIZE + index];
+        }
+    }
+
+    local_data[local_index] = thread_sum;
+    __syncthreads();
+
+    if (local_index != 0) {
+        return;
+    }
+    unsigned int block_sum = 0;
+    for (unsigned int i = 0; i < GROUP_SIZE; i++) {
+        block_sum += local_data[i];
+    }
+
+    b[block_index] = block_sum;
 }
 
 namespace cuda {
-void sum_04_local_reduction(const gpu::WorkSize &workSize,
-    const gpu::gpu_mem_32u &a, gpu::gpu_mem_32u &sum, unsigned int n)
+void sum_04_local_reduction(const gpu::WorkSize& workSize,
+    const gpu::gpu_mem_32u& a, gpu::gpu_mem_32u& sum, unsigned int n)
 {
     gpu::Context context;
     rassert(context.type() == gpu::Context::TypeCUDA, 6573652345243, context.type());
