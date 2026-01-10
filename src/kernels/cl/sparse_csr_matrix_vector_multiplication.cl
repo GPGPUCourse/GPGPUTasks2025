3b63badd562eb0536784442c230b28ab7249f442
Ekaterina <ekaterina_semchuk_5@mail.ru>
2026-01-11 00:48:26 +0300

    Task07 Екатерина Семчук ITMO

--- src/kernels/cl/sparse_csr_matrix_vector_multiplication.cl
+++ src/kernels/cl/sparse_csr_matrix_vector_multiplication.cl
@@ -5,8 +5,43 @@
 #include "helpers/rassert.cl"
 #include "../defines.h"
 
-__attribute__((reqd_work_group_size(1, 1, 1)))
-__kernel void sparse_csr_matrix_vector_multiplication() // TODO input/output buffers
+__attribute__((reqd_work_group_size(GROUP_SIZE, 1, 1)))
+__kernel void sparse_csr_matrix_vector_multiplication(
+    __global const uint* csr_row_offsets,
+    __global const uint* csr_columns,
+    __global const uint* csr_values,
+    __global const uint* vector_values,
+    __global uint* output_vector_values,
+    unsigned int  nrows,
+    unsigned int  nnz
+)
 {
-    // TODO
+    const uint index = get_local_id(0);
+    const uint row = get_group_id(0);
+    
+    __local uint values[GROUP_SIZE];
+
+    if (row < nrows) {
+        const uint offset = csr_row_offsets[row];
+        const uint csr_index = offset + index;
+        const uint offset_next = (row + 1 < nrows) ? csr_row_offsets[row + 1] : nnz;
+
+        if (csr_index < offset_next) {
+            const uint col = csr_columns[csr_index];
+            values[index] = csr_values[csr_index] * vector_values[col];
+        }
+        else {
+            values[index] = 0;
+        }
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (index > 0 || row >= nrows) return;
+    
+    uint res = 0;
+    for (uint i = 0; i < GROUP_SIZE; i++) {
+        res += values[i];
+    }
+    output_vector_values[row] = res;
 }
--- src/main_csr_spmv.cpp
+++ src/main_csr_spmv.cpp
@@ -123,7 +123,7 @@ void run(int argc, char** argv)
         std::cout << "____________________________________________________________________________________________" << std::endl;
         const auto [csr_row_offsets, csr_columns, csr_values] = generate_csr_matrix(nrows, ncols, min_nnz_per_row, max_nnz_per_col, max_value);
         const std::vector<unsigned int> vector_values = generate_vector(ncols, max_value);
-        const unsigned int number_of_non_zero_values = csr_row_offsets[nrows];
+        const unsigned int number_of_non_zero_values = csr_row_offsets[nrows];        
         const unsigned int nnz = number_of_non_zero_values;
 
         std::vector<unsigned int> nnz_per_row(nrows);
@@ -157,23 +157,13 @@ void run(int argc, char** argv)
 
         // Запускаем кернел (несколько раз и с замером времени выполнения)
         std::vector<double> times;
-        for (int iter = 0; iter < 10; ++iter) { // TODO при отладке запускайте одну итерацию
+        gpu::WorkSize workSize(GROUP_SIZE, GROUP_SIZE * ncols);
+
+        for (int iter = 0; iter < 10; ++iter) {
             t.restart();
 
             // Запускаем кернел, с указанием размера рабочего пространства и передачей всех аргументов
-            // Если хотите - можете удалить ветвление здесь и оставить только тот код который соответствует вашему выбору API
-            if (context.type() == gpu::Context::TypeOpenCL) {
-                // TODO
-                throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-            } else if (context.type() == gpu::Context::TypeCUDA) {
-                // TODO
-                throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-            } else if (context.type() == gpu::Context::TypeVulkan) {
-                // TODO
-                throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
-            } else {
-                rassert(false, 4531412341, context.type());
-            }
+            ocl_spvm.exec(workSize, csr_row_offsets_gpu, csr_columns_gpu, csr_values_gpu, vector_values_gpu, output_vector_values_gpu, nrows, nnz);
 
             times.push_back(t.elapsed());
         }
