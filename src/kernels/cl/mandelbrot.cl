8601630cf26b1ef21550a7080ebb0b5b69716baa
Denis Sokolov <denchika02@yandex.ru>
2026-03-02 06:12:33 +0300

    init hw

--- src/kernels/cl/mandelbrot.cl
+++ src/kernels/cl/mandelbrot.cl
@@ -15,5 +15,35 @@ __kernel void mandelbrot(__global float* results,
     const unsigned int i = get_global_id(0);
     const unsigned int j = get_global_id(1);
 
-    // TODO
+    if (i >= width || j >= height) return;
+
+    const unsigned int idx = j * width + i;
+
+    const float threshold = 256.0f;
+    const float threshold2 = threshold * threshold;
+
+    float x0 = fromX + (i + 0.5f) * sizeX / width;
+    float y0 = fromY + (j + 0.5f) * sizeY / height;
+
+    float x = x0;
+    float y = y0;
+
+    int iter = 0;
+    for (; iter < iters; ++iter) {
+        float xn = x * x - y * y + x0;
+        y = 2.0f * x * y + y0;
+        x = xn;
+
+        if (x * x + y * y > threshold2) {
+            break;
+        }
+    }
+
+    float res = iter;
+    if (isSmoothing && iter != iters) {
+        res = res - log(log(sqrt(x * x + y * y)) / log(threshold)) / log(2.0f);
+    }
+
+    res = 1.0f * res / iters;
+    results[idx] = res;
 }
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -72,10 +72,18 @@ void run(int argc, char** argv)
     gpu::gpu_mem_32u reduction_buffer2_gpu(div_ceil(n, (unsigned int)GROUP_SIZE));
 
     // Прогружаем входные данные по PCI-E шине: CPU RAM -> GPU VRAM
-    input_gpu.writeN(values.data(), n);
-    // TODO 1) замерьте здесь какая достигнута пропускная пособность PCI-E шины
-    // TODO 2) сделайте замер хотя бы три раза
-    // TODO 3) и выведите рассчет на основании медианного времени (в легко понятной форме - GB/s)
+
+    std::vector<double> times;
+    for (int iter = 0; iter < 10; ++iter) {
+        timer t;
+        input_gpu.writeN(values.data(), n);
+        times.push_back(t.elapsed());
+    }
+
+    std::cout << "PCI-E: CPU RAM -> GPU VRAM times (in seconds) - " << stats::valuesStatsLine(times) << std::endl;
+
+    double memory_size_gb = sizeof(unsigned int) * n / 1024.0 / 1024.0 / 1024.0;
+    std::cout << "PCI-E: CPU RAM -> GPU VRAM median VRAM bandwidth: " << memory_size_gb / stats::median(times) << " GB/s" << std::endl;
 
     std::vector<std::string> algorithm_names = {
         "CPU",
@@ -113,11 +121,21 @@ void run(int argc, char** argv)
                         ocl_sum02AtomicsLoadK.exec(gpu::WorkSize(GROUP_SIZE, n / LOAD_K_VALUES_PER_ITEM), input_gpu, sum_accum_gpu, n);
                         sum_accum_gpu.readN(&gpu_sum, 1);
                     } else if (algorithm == "03 local memory and atomicAdd from master thread") {
-                        // TODO ocl_sum03LocalMemoryAtomicPerWorkgroup.exec(...);
-                        throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
+                        sum_accum_gpu.fill(0);
+                        ocl_sum03LocalMemoryAtomicPerWorkgroup.exec(gpu::WorkSize(GROUP_SIZE, n), input_gpu, sum_accum_gpu, n);
+                        sum_accum_gpu.readN(&gpu_sum, 1);
                     } else if (algorithm == "04 local reduction") {
-                        // TODO ocl_sum04LocalReduction.exec(...);
-                        throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
+                        unsigned int size = n;
+                        reduction_buffer2_gpu.fill(0);
+                        ocl_sum04LocalReduction.exec(gpu::WorkSize(GROUP_SIZE, size), input_gpu, reduction_buffer2_gpu, size);
+                        size = div_ceil(size, (unsigned int)GROUP_SIZE);
+                        while (size > 1) {
+                            reduction_buffer1_gpu.swap(reduction_buffer2_gpu);
+                            reduction_buffer2_gpu.fill(0);
+                            ocl_sum04LocalReduction.exec(gpu::WorkSize(GROUP_SIZE, size), reduction_buffer1_gpu, reduction_buffer2_gpu, size);
+                            size = div_ceil(size, (unsigned int)GROUP_SIZE);
+                        }
+                        reduction_buffer2_gpu.readN(&gpu_sum, 1);
                     } else {
                         rassert(false, 652345234321, algorithm, algorithm_index);
                     }
